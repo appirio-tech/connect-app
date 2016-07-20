70b5c3bf6823b83614e9337a5f67feab13d13832
vikasrohit <vikas.agarwal@appirio.com>
2016-07-20 13:07:49 +0530

    AS#148343855905800, Tool bar - All Projects (dashboard) -- Removed table layout from projects list component, rather we would use simple list of components

--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -1,60 +1,18 @@
 import React, { PropTypes } from 'react'
-import {Table, Column, Cell} from 'fixed-data-table'
-// import Project from '../Project/Project'
+import Project from '../Project/Project'
 
 require('./ProjectList.scss')
-require('fixed-data-table/dist/fixed-data-table.css')
 
 const ProjectList = (({ projects }) => {
 
-  const renderName = ({rowIndex, ...props}) => (
-    <Cell {...props}>
-      { projects[rowIndex].name }
-    </Cell>
-  )
-  const renderPhase = ({rowIndex, ...props}) => (
-    <Cell {...props}>
-      { projects[rowIndex].currentPhase }
-    </Cell>
-  )
-  const renderStartsOn = ({rowIndex, ...props}) => (
-    <Cell {...props}>
-      { projects[rowIndex].startsOn }
-    </Cell>
-  )
-  const projectsDOM = (
-    <Table
-      rowHeight={50}
-      rowsCount={projects.length}
-      width={900}
-      height={600}
-      headerHeight={50}
-    >
-      <Column
-        header={<Cell>Name</Cell>}
-        cell={ renderName }
-        flexGrow={3}
-        width={100}
-      />
-      <Column
-        header={<Cell>Phase</Cell>}
-        cell={ renderPhase }
-        flexGrow={1}
-        width={100}
-      />
-      <Column
-        header={<Cell>Starts On</Cell>}
-        cell={ renderStartsOn }
-        flexGrow={1}
-        width={100}
-      />
-    </Table>
-  )
+  const renderProject = (project, idx) => {
+    return (<Project project={ project } key={ idx } />)
+  }
 
 
   return (
     <div className="project-list">
-      { projectsDOM }
+      { projects.map(renderProject) }
     </div>
   )
 })
