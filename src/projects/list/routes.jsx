d336042555b2f4d285ad78cf52106bd3932dca17
vikasrohit <vikas.agarwal@appirio.com>
2016-07-20 15:13:42 +0530

    AS#148343855905800, Tool bar - All Projects (dashboard) -- Fixed require paths

--- src/projects/list/routes.jsx
+++ src/projects/list/routes.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Route } from 'react-router'
 
-import Projects    from './components/Projects/Projects'
+import Projects from './components/Projects/Projects'
 
 const projectsListRoutes = (
-  <Route path="/projects/" component={ Projects } />
+  <Route path="/projects" component={ Projects } />
 )
 
 export default projectsListRoutes
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import Route from 'react-router/lib/Route'
 
 import App             from './components/App/App'
-import Projects    from './components/Projects/Projects'
 
 import projectDetailRoutes from './projects/detail/routes.jsx'
 import projectsListRoutes from './projects/list/routes.jsx'
 
 export default (
   <Route path="/" component={ App }>
-    <Route path="projects" component={ Projects } />
     {projectDetailRoutes}
     {projectsListRoutes}
   </Route>
