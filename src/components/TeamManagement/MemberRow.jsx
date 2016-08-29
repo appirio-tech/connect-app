a4659c0922c8fc989ce6544c49d9e27331a5554f
Parth Shah <parth@topcoder.com>
2016-08-29 23:19:54 +0530

    fixing icons

--- src/components/TeamManagement/MemberRow.jsx
+++ src/components/TeamManagement/MemberRow.jsx
@@ -1,8 +1,6 @@
 import React, {PropTypes} from 'react'
 import cn from 'classnames'
 import { Icons, Avatar } from 'appirio-tech-react-components'
-import IconPromote from './icons/IconPromote'
-
 
 const ActionBtn = (props) => {
   let icon
@@ -14,7 +12,7 @@ const ActionBtn = (props) => {
     icon = <Icons.IconLeave />
     break
   case 'promote':
-    icon = <IconPromote />
+    icon = <Icons.IconArrowPriorityHigh />
     break
   }
   return (
