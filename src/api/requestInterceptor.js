d038d2cc2d9e357fdbb16767fe1d671cf3785aa5
Parth Shah <parth@topcoder.com>
2016-08-30 13:20:56 +0530

    fixing error with retrieving user token

--- src/api/requestInterceptor.js
+++ src/api/requestInterceptor.js
@@ -4,8 +4,9 @@ import { getFreshToken, isTokenExpired } from 'tc-accounts'
 
 const getToken = () => {
   return new Promise((resolve) => {
-    const token = store.getState().loadUser.user.token
-    if (!isTokenExpired(token)) {
+    const userState = store.getState().loadUser
+    const token = userState && userState.user ? userState.user.token : null
+    if (token && !isTokenExpired(token)) {
       return resolve(token)
     } else {
       return getFreshToken()
