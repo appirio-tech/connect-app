04d1e0425572deb2cbbfb2a730edf7c72609aef7
Parth Shah <parth@topcoder.com>
2016-09-03 15:06:11 +0530

    fixing userdropdown properties & member avatar

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -77,7 +77,7 @@ class TopBar extends Component {
           <div className="menu-wrap" onClick={this.handleMobileClick}>
             <div className="mobile-wrap"><a href={mobileMenuUrl}><IconTcMenuBold /></a></div>
             { quickLinks }
-            <UserDropdown username={username} userImage={userImage} domain={domain} menuItems={ userMenuItems } forReactRouter/>
+            <UserDropdown userHandle={username} userImage={userImage} domain={domain} menuItems={ userMenuItems } forReactRouter/>
           </div>
         </div>
       </div>
