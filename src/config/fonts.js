7bc8858c39e5b3dd720a0e4649de93d32dd9c43a
Victor George <vgeorge@appirio.com>
2016-09-12 15:16:38 -0700

    Added the Roboto webfont library to the project. Use the ```font-family: "Roboto";``` with ```font-weight: 100~900;```  properties.

---
+++ src/config/fonts.js
@@ -0,0 +1,32 @@
+{
+    "name": "Roboto",
+    "files": [{
+        "weight": 100,
+        "style": "normal",
+        "file": "./assets/fonts/roboto/roboto-100-thin/roboto-thin.eot"
+    }, {
+        "weight": 100,
+        "style": "italic",
+        "file": "./assets/fonts/roboto/roboto-100-thin/roboto-thin.eot"
+    }, {
+        "weight": 300,
+        "style": "normal",
+        "file": "./assets/fonts/roboto/roboto-300-light/roboto Light.otf"
+    }, {
+        "weight": 400,
+        "style": "normal",
+        "file": "./assets/fonts/roboto/roboto-400-regular/roboto Reg.otf"
+    }, {
+        "weight": 500,
+        "style": "normal",
+        "file": "./assets/fonts/roboto/roboto-500-medium/roboto Sbold.otf"
+    }, {
+        "weight": 700,
+        "style": "normal",
+        "file": "./assets/fonts/roboto/roboto-700-bold/roboto Bold.otf"
+    }, {
+        "weight": 900,
+        "style": "normal",
+        "file": "./assets/fonts/roboto/roboto-900-black/roboto Xbold.otf"
+    }]
+}
