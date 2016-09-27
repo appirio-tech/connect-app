9decca1884336daa494c89acfcd6e309f118af2d
mahestro <millanluiseduardo@gmail.com>
2016-09-27 16:15:00 +0200

    Added landing page #124

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
+import './Home.scss'
+import homeImgSrc from '../../assets/images/hero-tc-landing.png'
 
 class Home extends React.Component {
   constructor(props) {
@@ -15,7 +17,22 @@ class Home extends React.Component {
 
   render() {
     return (
-      <h3> New Connect Home </h3>
+      <div className="content">
+        <div className="container">
+            <div className="image-container">
+                <img src={homeImgSrc} alt="" />
+            </div>
+            <div className="content-container">
+                <h1>Welcome to Topcoder Connect </h1>
+                <p>Companies and agencies, from the world’s largest to the Valley’s newest, use crowdsourcing to deliver high-quality assets, faster.</p>
+                <p>Join Connect today to see what we can do for your next project through the power of crowdsourcing, delivered by Topcoder.</p>
+                <div className="button-bar">
+                    <a href="javascript:" className="tc-btn tc-btn-primary">Register for free</a>
+                    <a href="javascript:" className="tc-btn tc-btn-default">Learn more about croudsorcing</a>
+                </div>
+            </div>
+        </div>
+      </div>
     )
   }
 }
