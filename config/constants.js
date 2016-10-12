289d88b9fa23ae43ae8d07e52eaa3f5446cfadd3
Łukasz Sentkiewicz <sirmims@gmail.com>
2016-10-12 13:45:03 +0200

    remove appirio-tech-webpack-config dependency

---
+++ config/constants.js
@@ -0,0 +1,112 @@
+
+
+module.exports = function(ENV) {
+  const constants = {};
+  if (ENV === 'DEV') {
+    Object.assign(constants, {
+      API_URL: 'https://api.topcoder-dev.com/v3',
+      API_URL_V2: 'https://api.topcoder-dev.com/v2',
+      WORK_API_URL: 'https://api-work.topcoder-dev.com/v3',
+      INTERNAL_API_URL: 'https://internal-api.topcoder-dev.com/v3',
+      ASSET_PREFIX: 'https://s3.amazonaws.com/app.topcoder-dev.com/',
+      AUTH_API_URL: 'https://api.topcoder-dev.com/v3',
+      auth0Callback: 'https://api.topcoder-dev.com/pub/callback.html',
+      auth0Domain: 'topcoder-dev.auth0.com',
+      clientId: 'JFDo7HMkf0q2CkVFHojy3zHWafziprhT',
+      AUTH0_DOMAIN: 'topcoder-dev.auth0.com',
+      AUTH0_CLIENT_ID: 'JFDo7HMkf0q2CkVFHojy3zHWafziprhT',
+      domain: 'topcoder-dev.com',
+      DOMAIN: 'topcoder-dev.com',
+      ENV: 'DEV',
+      NEW_RELIC_APPLICATION_ID: process.env.TRAVIS_BRANCH ? '8957921' : '',
+      ARENA_URL: '//arena.topcoder-dev.com',
+      BLOG_LOCATION: 'https://www.topcoder-dev.com/feed/?post_type=blog',
+      COMMUNITY_URL: '//community.topcoder-dev.com',
+      FORUMS_APP_URL: '//apps.topcoder-dev.com/forums',
+      HELP_APP_URL: 'help.topcoder-dev.com',
+      MAIN_URL: 'https://www.topcoder-dev.com',
+      PHOTO_LINK_LOCATION: 'https://community.topcoder-dev.com',
+      SWIFT_PROGRAM_URL: 'apple.topcoder-dev.com',
+      TCO16_URL: 'http://tco16.topcoder-dev.com',
+      TCO17_URL: 'http://tco17.topcoder-dev.com',
+      TCO_HOME_URL: 'https://www.topcoder-dev.com/tco',
+      ACCOUNTS_APP_URL: 'https://accounts.topcoder-dev.com/#/member',
+      ACCOUNTS_APP_CONNECTOR_URL: 'https://accounts.topcoder-dev.com/connector.html',
+      FILE_PICKER_API_KEY: 'AzFINuQoqTmqw0QEoaw9az',
+      FILE_PICKER_SUBMISSION_CONTAINER_NAME: 'submission-staging-dev',
+      SALESFORCE_PROJECT_LEAD_LINK: 'https://appirio--dev.cs59.my.salesforce.com/apex/ConnectLead?connectProjectId='
+    });
+  }
+  if (ENV === 'QA') {
+    Object.assign(constants, {
+      API_URL: 'https://api.topcoder-qa.com/v3',
+      API_URL_V2: 'https://api.topcoder-qa.com/v2',
+      WORK_API_URL: 'https://api-work.topcoder-qa.com/v3',
+      INTERNAL_API_URL: 'https://internal-api.topcoder-qa.com/v3',
+      ASSET_PREFIX: 'https://s3.amazonaws.com/app.topcoder-qa.com/',
+      AUTH_API_URL: 'https://api.topcoder-qa.com/v3',
+      auth0Callback: 'https://api.topcoder-qa.com/pub/callback.html',
+      auth0Domain: 'topcoder-qa.auth0.com',
+      clientId: 'EVOgWZlCtIFlbehkq02treuRRoJk12UR',
+      AUTH0_DOMAIN: 'topcoder-qa.auth0.com',
+      AUTH0_CLIENT_ID: 'EVOgWZlCtIFlbehkq02treuRRoJk12UR',
+      domain: 'topcoder-qa.com',
+      DOMAIN: 'topcoder-qa.com',
+      ENV: 'QA',
+      NEW_RELIC_APPLICATION_ID: process.env.TRAVIS_BRANCH ? '11199233' : '',
+      ARENA_URL: '//arena.topcoder-qa.com',
+      BLOG_LOCATION: 'https://www.topcoder-qa.com/feed/?post_type=blog',
+      COMMUNITY_URL: '//community.topcoder-qa.com',
+      FORUMS_APP_URL: '//apps.topcoder-qa.com/forums',
+      HELP_APP_URL: 'help.topcoder-qa.com',
+      MAIN_URL: 'https://www.topcoder-qa.com',
+      PHOTO_LINK_LOCATION: 'https://community.topcoder-qa.com',
+      SWIFT_PROGRAM_URL: 'apple.topcoder-qa.com',
+      TCO16_URL: 'http://tco16.topcoder-qa.com',
+      TCO17_URL: 'http://tco17.topcoder-qa.com',
+      TCO_HOME_URL: 'https://www.topcoder-dev.com/tco',
+      ACCOUNTS_APP_URL: 'https://accounts.topcoder-qa.com/#/member',
+      ACCOUNTS_APP_CONNECTOR_URL: 'https://accounts.topcoder-qa.com/connector.html',
+      FILE_PICKER_API_KEY: 'ACrnuL2lqRAOOHLOhqwkaz',
+      FILE_PICKER_SUBMISSION_CONTAINER_NAME: 'submission-staging-qa',
+      SALESFORCE_PROJECT_LEAD_LINK: 'https://appirio--dev.cs59.my.salesforce.com/apex/ConnectLead?connectProjectId='
+    });
+  }
+  if (ENV === 'PROD') {
+    Object.assign(constants, {
+      API_URL: 'https://api.topcoder.com/v3',
+      API_URL_V2: 'https://api.topcoder.com/v2',
+      WORK_API_URL: 'https://api-work.topcoder.com/v3',
+      INTERNAL_API_URL: 'https://internal-api.topcoder.com/v3',
+      ASSET_PREFIX: 'https://s3.amazonaws.com/app.topcoder.com/',
+      AUTH_API_URL: 'https://api.topcoder.com/v3',
+      auth0Callback: 'https://api.topcoder.com/pub/callback.html',
+      auth0Domain: 'topcoder.auth0.com',
+      clientId: '6ZwZEUo2ZK4c50aLPpgupeg5v2Ffxp9P',
+      AUTH0_DOMAIN: 'topcoder.auth0.com',
+      AUTH0_CLIENT_ID: '6ZwZEUo2ZK4c50aLPpgupeg5v2Ffxp9P',
+      domain: 'topcoder.com',
+      DOMAIN: 'topcoder.com',
+      ENV: 'PROD',
+      NODE_ENV: 'production',
+      NEW_RELIC_APPLICATION_ID: process.env.TRAVIS_BRANCH ? '11352758' : '',
+      ARENA_URL: '//arena.topcoder.com',
+      BLOG_LOCATION: 'https://www.topcoder.com/feed/?post_type=blog',
+      COMMUNITY_URL: '//community.topcoder.com',
+      FORUMS_APP_URL: '//apps.topcoder.com/forums',
+      HELP_APP_URL: 'help.topcoder.com',
+      MAIN_URL: 'https://www.topcoder.com',
+      PHOTO_LINK_LOCATION: 'https://community.topcoder.com',
+      SWIFT_PROGRAM_URL: 'apple.topcoder.com',
+      TCO16_URL: 'http://tco16.topcoder.com',
+      TCO17_URL: 'http://tco17.topcoder.com',
+      TCO_HOME_URL: 'https://www.topcoder.com/tco',
+      ACCOUNTS_APP_URL: 'https://accounts.topcoder.com/#/member',
+      ACCOUNTS_APP_CONNECTOR_URL: 'https://accounts.topcoder.com/connector.html',
+      FILE_PICKER_API_KEY: 'ABqZ3MVqqSeiqL2fMOjTxz',
+      FILE_PICKER_SUBMISSION_CONTAINER_NAME: 'submission-staging-prod',
+      SALESFORCE_PROJECT_LEAD_LINK: 'https://appirio.my.salesforce.com/apex/ConnectLead?connectProjectId='
+    });
+  }
+  return constants;
+};
---
+++ config/webpack.config.js
@@ -0,0 +1,176 @@
+
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const constants = require('./constants');
+
+const branch = process.env.TRAVIS_BRANCH
+
+if (branch === 'master') process.env.ENV = 'PROD'
+if (branch === 'dev')    process.env.ENV = 'DEV'
+if (branch === 'qa')     process.env.ENV = 'QA'
+
+
+const dirname = path.join(__dirname, '..');
+const entry = {
+  app: [
+    './src/styles/main.scss',
+    './src/index',
+  ]
+};
+const template = path.join(dirname, './src/index.html');
+
+let TEST = false;
+let BUILD = false;
+let SILENT = false;
+let MOCK = false;
+let ENV = process.env.ENV || 'DEV';
+process.argv.forEach(function (arg) {
+  if (arg === '--test') {
+    TEST = true;
+  }
+  if (arg === '--build') {
+    BUILD = true;
+  }
+  if (arg === '--silent') {
+    SILENT = true;
+  }
+  if (arg === '--dev') {
+    ENV = 'DEV';
+  }
+  if (arg === '--qa') {
+    ENV = 'QA';
+  }
+  if (arg === '--prod') {
+    ENV = 'PROD';
+  }
+  if (arg === '--mock') {
+    return MOCK = true;
+  }
+});
+const envConstants = constants(ENV);
+if (!SILENT) {
+  console.log('Assigning the following constants to process.env:');
+  console.log(envConstants);
+  console.log('\n');
+}
+Object.assign(process.env, envConstants);
+const config = {
+  entry,
+  context: dirname,
+};
+
+if (TEST) {
+  config.output = {};
+} else {
+  config.output = {
+    path: path.join(dirname, '/dist'),
+    filename: '[name].[hash].js',
+    chunkFilename: '[name].[hash].js'
+  };
+}
+if (TEST) {
+  config.devtool = 'inline-source-map';
+} else if (BUILD) {
+  config.devtool = 'source-map';
+} else {
+  config.devtool = 'eval';
+}
+const scssLoaderBase = 'css-loader!sass-loader';
+const scssLoader = BUILD ? ExtractTextPlugin.extract('style-loader', scssLoaderBase) : 'style-loader!' + scssLoaderBase;
+config.module = {
+  preLoaders: [],
+  loaders: [
+    {
+      test: /\.(js|jsx)$/,
+      loaders: [
+        'react-hot',
+        'babel?' + JSON.stringify({
+          presets: [ 'es2015', 'react', 'stage-2' ],
+          plugins: [ 'lodash' ]
+        })
+      ],
+      exclude: /node_modules\/(?!appirio-tech.*|topcoder|tc-)/,
+    }, {
+      test: /^(?!.*\.react\.jade$)(.*\.jade$)/,
+      loader: 'jade'
+    }, {
+      test: /\.react\.jade$/,
+      loader: 'jade-react'
+    }, {
+      test: /\.jader$/,
+      loader: 'jade-react'
+    }, {
+      test: /\.(coffee|litcoffee|cjsx)$/,
+      loader: 'babel?presets[]=react,presets[]=es2015,presets[]=stage-2,plugins[]=lodash!coffee!cjsx'
+    }, {
+      test: /\.json$/,
+      loader: 'json'
+    }, {
+      test: /\.scss$/,
+      loader: scssLoader
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+    }, {
+      test: /\.(png|jpg|jpeg|gif)$/,
+      loader: 'file'
+    }, {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'file'
+    }, {
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'file'
+    }
+  ],
+  postLoaders: [
+    {
+      test: /\.(js|coffee|cjsx|jsx)$/,
+      loader: 'transform/cacheable?envify'
+    }
+  ]
+};
+config.resolveLoader = {
+  root: path.join(dirname, '/node_modules/')
+};
+config.resolve = {
+  root: path.join(dirname, '/node_modules/'),
+  modulesDirectories: ['node_modules'],
+  extensions: ['', '.js', '.jsx', '.json', '.coffee', '.jade', '.jader', '.scss', '.svg', '.png', '.gif', '.jpg', '.cjsx']
+};
+config.sassLoader = {
+  includePaths: [path.join(dirname, '/node_modules/bourbon/app/assets/stylesheets'), path.join(dirname, '/node_modules/tc-ui/src/styles')]
+};
+config.plugins = [];
+config.plugins.push(new ExtractTextPlugin('[name].[hash].css'));
+config.plugins.push(new webpack.DefinePlugin({
+  __MOCK__: JSON.stringify(JSON.parse(MOCK || 'false'))
+}));
+if (!TEST) {
+  config.plugins.push(new HtmlWebpackPlugin({
+    template: './src/index.html',
+    inject: 'body',
+    favicon: null,
+    NEW_RELIC_APPLICATION_ID: process.env.NEW_RELIC_APPLICATION_ID
+  }));
+}
+if (BUILD) {
+  config.plugins.push(new webpack.IgnorePlugin(/\.mock\.js/));
+  config.plugins.push(new webpack.NoErrorsPlugin());
+  config.plugins.push(new webpack.optimize.DedupePlugin());
+  const uglifyOptions = {
+    mangle: true
+  };
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin(uglifyOptions));
+  config.plugins.push(new CompressionPlugin({
+    asset: "{file}",
+    algorithm: "gzip",
+    regExp: /\.js$|\.css$/,
+    threshold: 10240,
+    minRatio: 0.8
+  }));
+}
+
+module.exports = config;
