ef57dc30418f396a3633008f6184caf19e10ed3b
vikasrohit <vikas.agarwal@appirio.com>
2016-10-14 11:30:28 +0530

    Github issue #244 [Project Dashbord] No confirmation on project status change -- Added modal for confirmation -- Pending item is to close the modal after successful status update

--- src/components/ProjectStatus/ProjectStatus.js
+++ src/components/ProjectStatus/ProjectStatus.js
@@ -1,17 +1,24 @@
 import React, {PropTypes} from 'react'
 import ReactDOM from 'react-dom'
+import Modal from 'react-modal'
 import PanelProject from '../PanelProject/PanelProject'
+import ProjectStatusChangeConfirmation from './ProjectStatusChangeConfirmation'
 import './ProjectStatus.scss'
 import cn from 'classnames'
 import _ from 'lodash'
 import uncontrollable from 'uncontrollable'
+import { Icons } from 'appirio-tech-react-components'
 import { PROJECT_ROLE_COPILOT, PROJECT_ROLE_MANAGER, PROJECT_STATUS } from '../../config/constants'
 
 class ProjectStatus extends React.Component {
 
   constructor(props) {
     super(props)
+    this.state = { showStatusChangeDialog : false }
     this.onClickOutside = this.onClickOutside.bind(this)
+    this.hideStatusChangeDialog = this.hideStatusChangeDialog.bind(this)
+    this.showStatusChangeDialog = this.showStatusChangeDialog.bind(this)
+    this.changeStatus = this.changeStatus.bind(this)
   }
 
   componentDidMount() {
@@ -30,50 +37,72 @@ class ProjectStatus extends React.Component {
     onToggleOpen(false, e)
   }
 
+  showStatusChangeDialog(newStatus, e) {
+    if (newStatus === 'completed' || newStatus === 'cancelled') {
+      this.setState({  newStatus, showStatusChangeDialog : true })
+    } else {
+      this.setState({  newStatus, showStatusChangeDialog : true })
+      // this.props.onChangeStatus(newStatus)
+    }
+  }
+
+  hideStatusChangeDialog() {
+    this.setState({ showStatusChangeDialog : false })
+  }
+
+  changeStatus() {
+    this.props.onChangeStatus(this.state.newStatus)
+  }
+
   render() {
-    const {directLinks, status, isOpen, currentMemberRole, onToggleOpen, onChangeStatus} = this.props
+    const {directLinks, status, isOpen, currentMemberRole, onToggleOpen, onChangeStatus } = this.props
+    const { showStatusChangeDialog } = this.state
     const selected = PROJECT_STATUS.filter((opt) => opt.value === status)[0]
     const canEdit = currentMemberRole
       && _.indexOf([PROJECT_ROLE_COPILOT, PROJECT_ROLE_MANAGER], currentMemberRole) > -1
 
     return (
       <PanelProject>
-        <div className="project-status">
-          <PanelProject.Heading>
-            Status
-          </PanelProject.Heading>
-          <div
-            onClick={(e) => canEdit && onToggleOpen(!isOpen, e)}
-            ref="toggleBtn"
-            className={cn('status-label', selected.color, {active: isOpen, editable : canEdit})}
-          >
-            <i className="status-icon"/>
-            {selected.name}
+        <div className={cn('panel', 'project-status-wrapper', {'modal-active': showStatusChangeDialog})}>
+          <div className="modal-overlay"></div>
+          <div className="project-status">
+            <PanelProject.Heading>
+              Status
+            </PanelProject.Heading>
+            <div
+              onClick={(e) => canEdit && onToggleOpen(!isOpen, e)}
+              ref="toggleBtn"
+              className={cn('status-label', selected.color, {active: isOpen, editable : canEdit})}
+            >
+              <i className="status-icon"/>
+              {selected.name}
+            </div>
+            {isOpen && <dir className="status-dropdown">
+              <ul>
+                {PROJECT_STATUS.map((item) =>
+                  <li key={item.value}>
+                    <a
+                      href="javascript:"
+                      className={cn({active: item.value === status})}
+                      onClick={(e) => {
+                        this.showStatusChangeDialog(item.value, e)
+                        onToggleOpen(false, e)
+                      }}
+                    >
+                      <span className={item.color}><i className="status-icon"/></span>
+                      {item.name}
+                    </a>
+                  </li>)}
+              </ul>
+            </dir>}
           </div>
-          {isOpen && <dir className="status-dropdown">
+          { showStatusChangeDialog && <ProjectStatusChangeConfirmation onConfirm={ this.changeStatus } onCancel={ this.hideStatusChangeDialog } /> }
+          {directLinks && <div className="project-direct-links">
             <ul>
-              {PROJECT_STATUS.map((item) =>
-                <li key={item.value}>
-                  <a
-                    href="javascript:"
-                    className={cn({active: item.value === status})}
-                    onClick={(e) => {
-                      onChangeStatus(item.value, e)
-                      onToggleOpen(false, e)
-                    }}
-                  >
-                    <span className={item.color}><i className="status-icon"/></span>
-                    {item.name}
-                  </a>
-                </li>)}
+              {directLinks.map((link, i) => <li key={i}><a href={link.href} target="_blank" rel="noopener noreferrer">{link.name}</a></li>)}
             </ul>
-          </dir>}
+          </div>}
         </div>
-        {directLinks && <div className="project-direct-links">
-          <ul>
-            {directLinks.map((link, i) => <li key={i}><a href={link.href} target="_blank" rel="noopener noreferrer">{link.name}</a></li>)}
-          </ul>
-        </div>}
       </PanelProject>
     )
   }
---
+++ src/components/ProjectStatus/ProjectStatusChangeConfirmation.jsx
@@ -0,0 +1,30 @@
+import React, {PropTypes} from 'react'
+
+const ProjectStatusChangeConfirmation = ({ onCancel, onConfirm }) => {
+
+  return (
+    <div className="modal">
+      <div className="modal-title danger center">
+        You are about to close the project
+      </div>
+      <div className="modal-body">
+        <p className="message center">
+          This will permanently change the status your project. This action cannot be undone.
+        </p>
+
+        <div className="button-area">
+          <button className="tc-btn tc-btn-default tc-btn-sm btn-cancel" onClick={onCancel}>Cancel</button>
+          {' '}
+          <button className="tc-btn tc-btn-warning tc-btn-sm" onClick={onConfirm}>Confirm</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+ProjectStatusChangeConfirmation.propTypes = {
+  onCancel: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired
+}
+
+export default ProjectStatusChangeConfirmation
