ca69fd64191a98988b10b4e6665205bd026bdb98
vikasrohit <vikas.agarwal@appirio.com>
2016-10-17 12:16:10 +0530

    Github issue #244 [Project Dashbord] No confirmation on project status change -- Fixed modal styles to match with specs for left panel of the project specs page

--- src/components/ProjectInfo/DeleteProjectModal.js
+++ src/components/ProjectInfo/DeleteProjectModal.js
@@ -3,17 +3,16 @@ import React, {PropTypes} from 'react'
 const DeleteProjectModal = ({ onCancel, onConfirm}) => {
   return (
     <div className="modal delete-project-modal">
-      <div className="modal-title danger flex center">
+      <div className="modal-title danger">
         You're about to delete this project
       </div>
       <div className="modal-body">
-        <p className="message center">
+        <p className="message">
           Are you sure you want to delete this project? You will lose any information you have entered and will not be able to undo this action.
         </p>
 
         <div className="button-area flex center">
           <button className="tc-btn tc-btn-default tc-btn-sm btn-cancel" onClick={onCancel}>Cancel</button>
-          {' '}
           <button className="tc-btn tc-btn-warning tc-btn-sm" onClick={onConfirm}>Delete Project</button>
         </div>
       </div>
