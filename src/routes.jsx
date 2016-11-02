428d16d4158f9b050d55b142d91e224157f0ad58
Vikas Agarwal <vikas.agarwal@topcoder.com>
2016-11-02 13:03:34 +0530

    Github issue #509 Redirect Discourse messages to connectv2 -- Added redirect to the project details page for the URL /messages/{topicId}

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,7 +6,8 @@ import Home from './components/Home/Home'
 import PageError from './components/PageError/PageError'
 import projectRoutes from './projects/routes.jsx'
 import RedirectComponent from './components/RedirectComponent'
-import {ACCOUNTS_APP_LOGIN_URL} from './config/constants'
+import {ACCOUNTS_APP_LOGIN_URL, PROJECT_FEED_TYPE_PRIMARY, PROJECT_FEED_TYPE_MESSAGES } from './config/constants'
+import { getTopic } from './api/messages'
 
 // import reportsListRoutes from './reports/routes.jsx'
 
@@ -14,10 +15,36 @@ const LoginRedirect = withProps({
   redirectTo: `${ACCOUNTS_APP_LOGIN_URL}?retUrl=${window.location.protocol}//${window.location.hostname}${window.location.port ? ':' + window.location.port : ''}`
 })(RedirectComponent)
 
+const redirectToProject = (nextState, replace, callback) => {
+  const feedId = nextState.params.feedId
+  getTopic(feedId).then(resp => {
+    if (resp.topics && resp.topics.length > 0) {
+      const topic = resp.topics[0]
+      const projectId = topic.referenceId
+      if (topic.tag === PROJECT_FEED_TYPE_PRIMARY) {
+        replace(`/projects/${projectId}/`)
+      } else if (topic.tag === PROJECT_FEED_TYPE_MESSAGES) {
+        replace({
+          pathname: `/projects/${projectId}/discussions`,
+          state: { threadId : topic.id }
+        })
+      } else {
+        replace('/projects')
+      }
+    }
+    callback()
+  })
+  .catch(error => {
+    replace('/projects')
+    callback()
+  })
+}
+
 export default (
   <Route path="/" onUpdate={() => window.scrollTo(0, 0)} component={ App }>
     <IndexRoute component={Home} />
     <Route path="/login" component={LoginRedirect}/>
+    <Route path="/messages/:feedId" onEnter={ redirectToProject } />
 
     {/* Handle /projects/* routes */}
     {projectRoutes}
