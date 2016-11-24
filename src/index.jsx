69d96342f94a18596663360fd34a2dd205a90725
Vikas Agarwal <vikas.agarwal@topcoder.com>
2016-11-24 12:55:52 +0530

    Parameterized the heap analytics script

--- src/index.jsx
+++ src/index.jsx
@@ -8,12 +8,15 @@ import Router         from 'react-router/lib/Router'
 import store  from './config/store'
 import routes from './routes'
 import { TCEmitter } from './helpers'
-import { EVENT_ROUTE_CHANGE } from './config/constants'
+import { EVENT_ROUTE_CHANGE, HEAP_ANALYTICS_APP_ID } from './config/constants'
 
 const mountNode = document.getElementById('root')
 const onRouteChange = () => {
   TCEmitter.emit(EVENT_ROUTE_CHANGE, window.location.pathname)
 }
+
+window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+heap.load(HEAP_ANALYTICS_APP_ID);
 
 render((
   <Provider store={store}>
