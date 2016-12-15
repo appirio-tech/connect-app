7bdbaad03d9b44918c3b08be41a4ada4abf74855
Vikas Agarwal <vikas.agarwal@topcoder.com>
2016-12-15 12:13:06 +0530

    Github issue #372, [Pagination] - keep fixed pagination option displaying 5 pages. -- Updated the paginator to show maximum 5 pages in the list -- Found a bug with Tooltip component which is preventing the tooltip from being visible

--- src/components/Grid/PaginationBar.jsx
+++ src/components/Grid/PaginationBar.jsx
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react'
 import cn from 'classnames'
 import _ from 'lodash'
 import { branch, renderComponent } from 'recompose'
+import { Tooltip } from 'appirio-tech-react-components'
 
 const noop = () => <noscript />
 /**
@@ -26,6 +27,22 @@ const PagePill = ({pageNum, onPageClick, currentPageNum}) => {
   )
 }
 
+const GoToPagePill = ({minPage, maxPage, changePage}) => {
+  const handleChange = (evt) => {
+    console.log(evt.target)
+  }
+  return (
+    <li className="go-to-page-pill">
+      <Tooltip popMethod="click">
+        <div className="tooltip-target"><span>...</span></div>
+        <div className="tooltip-body">
+          <div>Go To: <input type="text" onChange={handleChange} /></div>
+        </div>
+      </Tooltip>
+    </li>
+  )
+}
+
 /**
  * Pagingation bar
  */
@@ -42,14 +59,45 @@ const PaginationBar = enhance(({onPageChange, currentPageNum, totalCount, pageSi
 
   // calculate number of pages to show
   const pages = Math.ceil(totalCount / pageSize)
+  const pagePills = []
+  const pagesBeforeCurrent = currentPageNum - 1 - 1
+  const pagesAfterCurrent = pages - currentPageNum - 1
+  if (currentPageNum > 1) {
+    pagePills.push(<PagePill key={1} pageNum={ 1 } onPageClick={handlePageSelection} currentPageNum={currentPageNum} />)
+  }
+  
+  pagePills.push(<PagePill key={currentPageNum} pageNum={ currentPageNum } onPageClick={handlePageSelection} currentPageNum={currentPageNum} />)
+  
+  if (currentPageNum < pages) {
+    pagePills.push(<PagePill key={pages} pageNum={ pages } onPageClick={handlePageSelection} currentPageNum={currentPageNum} />)
+  }
+  const remainingPillsLeft = Math.ceil((5 - pagePills.length) / 2)
+  
+  if (pagesBeforeCurrent > 0) {
+    for(let i=1; i <= remainingPillsLeft && i <= pagesBeforeCurrent; i++) {
+      pagePills.splice(1, 0, <PagePill key={ currentPageNum - i } pageNum={ currentPageNum - i } onPageClick={handlePageSelection} currentPageNum={currentPageNum} />)
+    }
+  }
+
+  const remainingPillsRight = 5 - pagePills.length
+  
+  if (pagesAfterCurrent > 0) {
+    for(let i=1; i <= remainingPillsRight && i <= pagesAfterCurrent; i++) {
+      pagePills.splice(pagePills.length - 1, 0, <PagePill key={ currentPageNum + i } pageNum={ currentPageNum + i } onPageClick={handlePageSelection} currentPageNum={currentPageNum} />)
+    }
+  }
+  if (pagesBeforeCurrent - remainingPillsLeft > 0) {
+    pagePills.splice(1, 0, <GoToPagePill />)
+  }
+  if (pagesAfterCurrent - remainingPillsRight > 0) {
+    pagePills.splice(pagePills.length - 1, 0, <GoToPagePill />)
+  }
   return (
     <div className="pages">
       <nav className="right-page">
         { currentPageNum > 1 && <a href="javascript:" onClick={handlePageSelection} className="btn-prev">Prev</a> }
         <ul>
-          { _.range(pages).map(idx =>
-            <PagePill key={idx} pageNum={idx+1} onPageClick={handlePageSelection} currentPageNum={currentPageNum} />
-          )}
+          { pagePills.map(pagePill => pagePill)}
         </ul>
         { currentPageNum < pages && <a href="javascript:" onClick={handlePageSelection} className="btn-next">Next</a> }
       </nav>
