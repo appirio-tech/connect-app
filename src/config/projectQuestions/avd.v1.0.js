57ece5d93082bcd293e4fe01808ea6ddd7543df2
canhnht <canhnht1709@gmail.com>
2017-02-15 10:02:57 +0700

    adding notes for app screens

--- src/config/projectQuestions/avd.v1.0.js
+++ src/config/projectQuestions/avd.v1.0.js
@@ -133,6 +133,13 @@ const sections = [
             ]
           }
         ]
+      },
+      {
+        id: 'notes',
+        fieldName: 'details.appScreens.notes',
+        title: 'Notes',
+        description: 'Add any other important information regarding your project (e.g., links to documents or existing applications, budget or timing constraints)',
+        type: 'notes'
       }
     ]              
   },
