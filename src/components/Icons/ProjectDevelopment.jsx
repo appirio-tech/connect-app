fef3e950df439977e0332f1b49b49964e6c81785
Victor George <vgeorge@appirio.com>
2017-02-21 18:39:09 -0800

    Added Yarn .lock file, and the appropriate (missing) .svg icons for the new project wizard

--- src/components/Icons/ProjectDevelopment.jsx
+++ src/components/Icons/ProjectDevelopment.jsx
@@ -1,8 +1,10 @@
 // TODO: Modify this component to use .svg icons instead of .png assets.
+// FIXME: This is wrong. We need to load only one .SVG in the main component. All states should be handled by CSS transformations only!
+
 
 import React from 'react'
-import Black from '../../assets/images/icon-project-development-black.png'
-import Blue from '../../assets/images/icon-project-development-blue.png'
+import Black from '../../assets/images/product-code.svg'
+import Blue from '../../assets/images/product-code.svg'
 
 function ProjectDevelopment(props) {
   const i = props.color === 'black' ? Black : Blue
--- src/components/Icons/ProjectPrototype.jsx
+++ src/components/Icons/ProjectPrototype.jsx
@@ -1,14 +1,16 @@
 // TODO: Modify this component to use .svg icons instead of .png assets.
+// FIXME: This is wrong. We need to load only one .SVG in the main component. All states should be handled by CSS transformations only!
+
 
 import React from 'react'
-import Black from '../../assets/images/icon-project-prototype-black.png'
-import Blue from '../../assets/images/icon-project-prototype-blue.png'
+import Icon from '../../assets/images/product-prototype.svg'
+// import Blue from '../../assets/images/icon-project-prototype-blue.png'
 
 function ProjectPrototype(props) {
-  const i = props.color === 'black' ? Black : Blue
+  const state = props.color === 'black' ? 'normal' : 'selected'
   const s = props.size
   return (
-    <img className="Icon ProjectPrototype" src={i} width={s} />
+    <img className={state + 'Icon ProjectPrototype'} src={Icon} width={s} />
   )
 }
 
