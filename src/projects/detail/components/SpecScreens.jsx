65aedd1c12b9b5cffed377cd13f9601f772e38d0
Vikas Agarwal <vikas.agarwal@topcoder.com>
2017-06-05 11:31:11 +0530

    Revert "Fixed lint error"
    This reverts commit 72f0f9e27771f16e5b10b49d6cde0d39abe4b109.

--- src/projects/detail/components/SpecScreens.jsx
+++ src/projects/detail/components/SpecScreens.jsx
@@ -57,7 +57,7 @@ class SpecScreens extends Component {
     const { screens } = this.state
     const { project } = this.props
     const numberOfScreens = _.get(project, 'details.appDefinition.numberScreens', '0')
-    const numberOfScreensValue = numberOfScreens.value ? numberOfScreens.value : numberOfScreens
+    let numberOfScreensValue = numberOfScreens.value ? numberOfScreens.value : numberOfScreens
     const numberScreensSelected = parseInt(_.last(numberOfScreensValue.split('-')))
 
     const renderCurrentScreen = (screen, index) => {
