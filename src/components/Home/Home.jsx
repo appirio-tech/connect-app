8b698a8b21e4adcbd3da7f416641f3fe7fb4710c
Vikas Agarwal <vikas.agarwal@topcoder.com>
2017-07-26 13:51:15 +0530

    Github issue#960, Update copy for new connect register flow in logged out project creation flow — Updated copy on connect home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,7 +18,7 @@ class Home extends React.Component {
   }
 
   render() {
-    const learnMoreUrl = 'https://www.' + DOMAIN +'/what-can-you-do/'
+    const learnMoreUrl = 'https://www.' + DOMAIN +'/about-topcoder/connect/'
     return (
       <div className="content content-home">
         <div className="container">
@@ -26,12 +26,12 @@ class Home extends React.Component {
                 <img src={homeImgSrc} alt="" />
             </div>
             <div className="content-container">
-                <h1>Welcome to Topcoder Connect </h1>
+                <h1>Topcoder Connect: the easiest way to go from idea to app</h1>
                 <p>Companies and agencies, from the world’s largest to the Valley’s newest, use crowdsourcing to deliver high-quality assets, faster.</p>
-                <p>Join Connect today to see what we can do for your next project through the power of crowdsourcing, delivered by Topcoder.</p>
+                <p>Connect guides you through the entire crowdsourcing process, from entering requirements to receiving final deliverables, and it facilitates collaboration between your project team and Topcoder community members working on your project.</p>
                 <div className="button-bar">
                     <Link to='new-project' className="tc-btn tc-btn-primary">Start a project</Link>
-                    <a href={ learnMoreUrl } className="tc-link">Learn more about crowdsourcing</a>
+                    <a href={ learnMoreUrl } className="tc-link">Learn more about Connect</a>
                 </div>
             </div>
         </div>
