b3df229ba360784c891fb34e29959d303afe5b28
davemess34 <davemess34@gmail.com>
2017-08-30 15:09:30 -0400

    removed REACT line to past linter

--- src/config/projectQuestions/crowd_testing.v1.0.js
+++ src/config/projectQuestions/crowd_testing.v1.0.js
@@ -1,5 +1,4 @@
 import _ from 'lodash'
-import React from 'react'
 import { Icons } from 'appirio-tech-react-components'
 import SVGIconImage from '../../components/SVGIconImage'
 // import NumberText from '../../components/NumberText/NumberText'
