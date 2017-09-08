887430fa7511cdad18f0fc82a5a029f1f66405d6
Vikas Agarwal <vikas.agarwal@topcoder.com>
2017-09-08 15:20:18 +0530

    Github issue#1114, Save incomplete project even if there is no change in form — Fixed.

--- src/projects/create/containers/CreateContainer.jsx
+++ src/projects/create/containers/CreateContainer.jsx
@@ -108,7 +108,7 @@ class CreateConainer extends React.Component {
   // stores the incomplete project in local storage
   onLeave(e) {// eslint-disable-line no-unused-vars
     const { wizardStep, isProjectDirty } = this.state
-    if (wizardStep === ProjectWizard.Steps.WZ_STEP_FILL_PROJ_DETAILS && isProjectDirty) {// Project Details step
+    if (wizardStep === ProjectWizard.Steps.WZ_STEP_FILL_PROJ_DETAILS) {// Project Details step
       console.log('saving incomplete project')
       window.localStorage.setItem(LS_INCOMPLETE_PROJECT, JSON.stringify(this.state.updatedProject))
     }
