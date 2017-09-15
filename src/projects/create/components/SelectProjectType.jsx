f675ff5c37e4a8dbe88da923b9fd7848e802923a
Vikas Agarwal <vikas.agarwal@topcoder.com>
2017-09-15 12:06:45 +0530

    Github issue#1170, Two tiered menus — Added topcoder logo to the select project type step

--- src/projects/create/components/SelectProjectType.jsx
+++ src/projects/create/components/SelectProjectType.jsx
@@ -5,6 +5,7 @@ import SVGIconImage from '../../../components/SVGIconImage'
 import './SelectProjectType.scss'
 
 function SelectProjectType(props) {
+  const { userRoles } = props
   const cards = []
   for (const key in config) {
     const item = config[key]
@@ -20,18 +21,24 @@ function SelectProjectType(props) {
     )
   }
   return (
-    <div className="SelectProjectType">
-      <h1>Select your project category</h1>
-      <div className="cards">{cards}</div>
-      <div className="footer">
-        Looking for something else? <a href="http://crowdsourcing.topcoder.com/piqued_by_crowdsourcing">Get in touch with us.</a>
+    <div>
+      <div className="header headerSelectProjectType">
+        { (!userRoles || !userRoles.length) && <SVGIconImage filePath="connect-logo-mono" />}
+      </div>
+      <div className="SelectProjectType">
+        <h1>Select your project category</h1>
+        <div className="cards">{cards}</div>
+        <div className="footer">
+          Looking for something else? <a href="http://crowdsourcing.topcoder.com/piqued_by_crowdsourcing">Get in touch with us.</a>
+        </div>
       </div>
     </div>
   )
 }
 
 SelectProjectType.propTypes = {
-  onProjectTypeChange: PT.func.isRequired
+  onProjectTypeChange: PT.func.isRequired,
+  userRoles: PT.arrayOf(PT.string)
 }
 
 export default SelectProjectType
