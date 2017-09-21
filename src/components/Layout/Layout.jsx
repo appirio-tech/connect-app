9a26eed242ba9973b896e73e3cc6eab6f997f1b8
Vikas Agarwal <vikas.agarwal@topcoder.com>
2017-09-21 12:28:22 +0530

    Github issue#1170, Two tiered menus — Misc UI changes to allow different background for new project flow

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from '../Footer/Footer'
 import { MAINTENANCE_MODE } from '../../config/constants'
 import Alert from 'react-s-alert'
+import cn from 'classnames'
 import Maintenance from '../Maintenance/Maintenance'
 
 require('./Layout.scss')
@@ -22,7 +23,7 @@ const Layout = (props) => {
       <div>
         { props.topbar }
         <Alert stack={{limit: 3, spacing: 30}} position="top" html timeout={4000} offset={0} />
-        <div className="main-wrapper" id="wrapper-main">
+        <div className={cn('main-wrapper')} data-route={window.location.pathname.replace(/\//g, "__")} id="wrapper-main">
           { props.content }
         </div>
         <Footer />
