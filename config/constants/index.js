c0fbfc62890ba853e2a0f1cb1147427176a527dc
Vikas Agarwal <vikas.agarwal@topcoder.com>
2017-11-06 16:56:12 +0530

    Fixed if else

--- config/constants/index.js
+++ config/constants/index.js
@@ -3,8 +3,7 @@ module.exports = (() => {
 
   // for security reason don't let to require any arbitrary file defined in process.env
   if (['master', 'qa'].indexOf(branch) < 0) {
-    return require('./' + branch)
-  } else {
     return require('./dev')
   }
+  return require('./' + branch)
 })()
