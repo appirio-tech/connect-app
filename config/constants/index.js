fbbb74b132f573cdea569224a648c47623ba7bc5
Samir Gondzetovic <gondzo@gmail.com>
2017-11-27 12:43:24 +0000

    fix config branch

--- config/constants/index.js
+++ config/constants/index.js
@@ -1,9 +1,9 @@
 module.exports = (() => {
-  const branch = process.env.CIRCLE_BRANCH || 'dev'
+  let branch = process.env.CIRCLE_BRANCH || 'dev'
 
   // for security reason don't let to require any arbitrary file defined in process.env
   if (['dev', 'master', 'qa'].indexOf(branch) < 0) {
-    throw Error('Unsupported CIRCLE_BRANCH value.')
+    branch='dev'
   }
 
   return require('./' + branch)
