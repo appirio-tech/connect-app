a5c4193cd7a9d3e833f21a54466d2d2706f6b3e4
Samir Gondzetovic <gondzo@gmail.com>
2017-12-22 02:57:52 +0000

    fix path

--- deploy.sh
+++ deploy.sh
@@ -26,8 +26,8 @@ deploy_s3bucket() {
 		S3_CACHE_OPTIONS="--cache-control max-age=0,s-maxage=86400"
 	fi
 
-	aws s3 sync --dryrun /home/osboxes/notifications/connect-app  --exclude "*.txt" --exclude "*.js" --exclude "*.css" ${S3_CACHE_OPTIONS}
-	result=$(aws s3 sync /home/osboxes/notifications/connect-app  --exclude "*.txt" --exclude "*.js" --exclude "*.css" ${S3_CACHE_OPTIONS})
+	aws s3 sync --dryrun ${HOME}/${CIRCLE_PROJECT_REPONAME}/dist s3://${AWS_S3_BUCKET}  --exclude "*.txt" --exclude "*.js" --exclude "*.css" ${S3_CACHE_OPTIONS}
+	result=$(aws s3 sync ${HOME}/${CIRCLE_PROJECT_REPONAME}/dist s3://${AWS_S3_BUCKET}  --exclude "*.txt" --exclude "*.js" --exclude "*.css" ${S3_CACHE_OPTIONS})
 	if [ $? -eq 0 ]; then
 		echo "All html, font, image, map and media files are Deployed without gzip encoding!"
 	else
@@ -35,8 +35,8 @@ deploy_s3bucket() {
 		exit 1
 	fi
 
-	aws s3 sync --dryrun /home/osboxes/notifications/connect-app --exclude "*" --include "*.txt" --include "*.js" --include "*.css" --content-encoding gzip ${S3_CACHE_OPTIONS}
-	result=$(aws s3 sync /home/osboxes/notifications/connect-app --exclude "*" --include "*.txt" --include "*.js" --include "*.css" --content-encoding gzip ${S3_CACHE_OPTIONS})
+	aws s3 sync --dryrun ${HOME}/${CIRCLE_PROJECT_REPONAME}/dist s3://${AWS_S3_BUCKET} --exclude "*" --include "*.txt" --include "*.js" --include "*.css" --content-encoding gzip ${S3_CACHE_OPTIONS}
+	result=$(aws s3 sync ${HOME}/${CIRCLE_PROJECT_REPONAME}/dist s3://${AWS_S3_BUCKET} --exclude "*" --include "*.txt" --include "*.js" --include "*.css" --content-encoding gzip ${S3_CACHE_OPTIONS})
 	if [ $? -eq 0 ]; then
 		echo "All txt, css, and js files are Deployed! with gzip"
 	else
