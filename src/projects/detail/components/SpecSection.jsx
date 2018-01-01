0e97e28872769399460b337ed6138ccf5cbf9a5a
Thomas Kranitsas <kranitsasthomas@gmail.com>
2018-01-01 21:45:47 +0200

    Fix auto-resize issue in textbox

--- src/projects/detail/components/SpecSection.jsx
+++ src/projects/detail/components/SpecSection.jsx
@@ -75,6 +75,7 @@ const SpecSection = props => {
             {props.description}
           </div>
           <TCFormFields.Textarea
+            autoResize
             name={props.fieldName}
             value={_.get(project, props.fieldName) || ''}
           />
