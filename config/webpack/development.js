3a98c6684f4b53587dfd1b5a5bc28574c0ec1d29
kevinkid <bigkevin2682@gmail.com>
2018-01-17 23:55:00 +0300

    Update development.js

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -26,6 +26,7 @@ module.exports = webpackMerge.strategy({
         plugins: [
           'lodash',
           // add react hot reloader
+         'react-hot-loader/babel',
           'inline-react-svg'
         ]
       }
