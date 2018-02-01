456e64d03166996a295baa5dabb2eaf0f42bc38b
Maksym Mykhailenko <maxceem@gmail.com>
2018-02-01 16:23:19 +0800

    removed console.log

--- config/babel/webpack-coffee.js
+++ config/babel/webpack-coffee.js
@@ -9,6 +9,4 @@ const topCoderBabelConfig = require('topcoder-react-utils/config/babel/webpack')
 const envPresetIndex = topCoderBabelConfig.presets.find((preset) => preset === 'env')
 topCoderBabelConfig.presets.splice(envPresetIndex, 1, ['env', { modules: false }])
 
-console.log('babel', JSON.stringify(topCoderBabelConfig, null, 4))
-
 module.exports = topCoderBabelConfig
