e03af1cb1018f9022ed127916fc66233e52c4015
Unknown <mtwomey@beakstar.com>
2018-02-20 18:34:13 -0600

    Test Connect with new accounts-app

--- config/constants/index.js
+++ config/constants/index.js
@@ -2,7 +2,7 @@ module.exports = (() => {
   const branch = process.env.CIRCLE_BRANCH || 'dev'
 
   // for security reason don't let to require any arbitrary file defined in process.env
-  if (['master', 'qa'].indexOf(branch) < 0) {
+  if (['master', 'qa', 'dev-auth0'].indexOf(branch) < 0) {
     return require('./dev')
   }
 
