e169417f524d07bd0e854244044999bbf5d39653
Unknown <mtwomey@beakstar.com>
2018-02-20 23:27:10 -0600

    lint

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,10 @@ module.exports = function buildConfig(env) {
 
   // Remove and re-instantiate UglifyJsPlugin if it was present (needed for source-maps)
   // See here: https://github.com/webpack/webpack/issues/2704
-  let uglifyPluginIndex = webpackConfig.plugins.findIndex(plugin => plugin.constructor.name === 'UglifyJsPlugin')
+  const uglifyPluginIndex = webpackConfig.plugins.findIndex(plugin => plugin.constructor.name === 'UglifyJsPlugin')
   if (uglifyPluginIndex > -1) {
-  	webpackConfig.plugins = webpackConfig.plugins.filter((plugin) => plugin.constructor.name !== 'UglifyJsPlugin')
-  	webpackConfig.plugins.splice(uglifyPluginIndex, 0, new UglifyJsPlugin({sourceMap: true}))
+    webpackConfig.plugins = webpackConfig.plugins.filter((plugin) => plugin.constructor.name !== 'UglifyJsPlugin')
+    webpackConfig.plugins.splice(uglifyPluginIndex, 0, new UglifyJsPlugin({sourceMap: true}))
   }
 
   return webpackConfig
