a7d4d24375af8ff6c3fdf58c8ef08af4d74e898f
Unknown <mtwomey@beakstar.com>
2018-02-20 23:23:50 -0600

    Fix source-maps generation

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,19 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
 
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
 module.exports = function buildConfig(env) {
   const webpackConfig = require(`./config/webpack/${env}.js`)
+
+  // Remove and re-instantiate UglifyJsPlugin if it was present (needed for source-maps)
+  // See here: https://github.com/webpack/webpack/issues/2704
+  let uglifyPluginIndex = webpackConfig.plugins.findIndex(plugin => plugin.constructor.name === 'UglifyJsPlugin')
+  if (uglifyPluginIndex > -1) {
+  	webpackConfig.plugins = webpackConfig.plugins.filter((plugin) => plugin.constructor.name !== 'UglifyJsPlugin')
+  	webpackConfig.plugins.splice(uglifyPluginIndex, 0, new UglifyJsPlugin({sourceMap: true}))
+  }
+
   return webpackConfig
 }
 
