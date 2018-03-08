aee40e6469dae0cb504a31d42443c3e844824c56
iversonLv <iverson_3_3_3@126.com>
2018-03-08 13:28:33 +0800

    Issue #1766 - Post feed->Create new post->User do not have option to cancel the post

--- src/components/RichTextArea/RichTextArea.jsx
+++ src/components/RichTextArea/RichTextArea.jsx
@@ -366,11 +366,9 @@ class RichTextArea extends React.Component {
                   }
                 </div>
                 <div className="tc-btns">
-                  { editMode && !isCreating &&
-                <button className="tc-btn tc-btn-link tc-btn-sm" onClick={this.cancelEdit}>
-                  Cancel
-                </button>
-                  }
+                  <button className="tc-btn tc-btn-link tc-btn-sm" onClick={this.cancelEdit}>
+                    Cancel
+                  </button>
                   { editMode &&
                 <button className="tc-btn tc-btn-primary tc-btn-sm" onClick={this.onPost} disabled={!canSubmit }>
                   { isCreating ? 'Saving...' : 'Save changes' }
