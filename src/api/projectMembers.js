b608f66811c59e989acc5aff231e3dfb19fa9e88
Vikas Agarwal <vikas.agarwal@topcoder.com>
2018-04-18 15:32:51 +0530

    Removed logic of calling syncUsers after team member operations.

--- src/api/projectMembers.js
+++ src/api/projectMembers.js
@@ -30,9 +30,7 @@ export function addProjectMember(projectId, newMember) {
   const url = `${PROJECTS_API_URL}/v4/projects/${projectId}/members/`
   return axios.post(url, { param: newMember})
     .then(resp => {
-      return axios.put(`${CONNECT_MESSAGE_API_URL}/v4/topics/syncUsers`,
-        { reference: 'project', referenceId: projectId })
-        .then(() => resp.data.result.content)
+      return resp.data.result.content
     })
 }
 
@@ -47,21 +45,9 @@ export function updateProjectMember(projectId, memberId, updatedProps) {
 
 export function removeProjectMember(projectId, memberId, isUserLeaving) {
   const url = `${PROJECTS_API_URL}/v4/projects/${projectId}/members/${memberId}/`
-  let promise = { then: fn => fn() }
-  if (isUserLeaving) {
-    promise = axios.put(`${CONNECT_MESSAGE_API_URL}/v4/topics/syncUsers`,
-      { reference: 'project', referenceId: projectId, isUserLeaving })
-  }
-  return promise.then(() => {
-    return axios.delete(url)
-      .then(() => {
-        if (!isUserLeaving) {
-          return axios.put(`${CONNECT_MESSAGE_API_URL}/v4/topics/syncUsers`,
-            { reference: 'project', referenceId: projectId })
-            .then(() => memberId)
-        }
-        // return the member id just removed
-        return memberId
-      })
-  })
+  return axios.delete(url)
+    .then(() => {
+      // return the member id just removed
+      return memberId
+    })
 }
