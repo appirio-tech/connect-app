a5bbf9da9ad224631289a7cb8048ad6c3e7e475f
Vikas Agarwal <vikas.agarwal@topcoder.com>
2018-06-20 12:58:23 +0530

    fixing toggle of other phase cards

--- src/projects/detail/components/PhaseCard/PhaseCard.jsx
+++ src/projects/detail/components/PhaseCard/PhaseCard.jsx
@@ -42,7 +42,12 @@ class PhaseCard extends React.Component {
   componentWillReceiveProps(nextProps) {
     // update phase finished successfully
     if(nextProps.isUpdating === false && this.props.isUpdating === true) {
-      this.toggleEditView()
+      // NOTE: following condition would be true for all stages after user updates only one of them
+      // and we don't have phase id with update phase action reducer so we can't determine which card is updated
+      // so we close all the edit forms for now
+      this.setState({
+        isEditting: false
+      })
     }
   }
 
