2a4ec86f6c4caa874d7017b0396ed3845d146d02
sumitdaga <sumit.daga66@gmail.com>
2018-06-23 22:30:19 +0530

    bundling posts - mobile

--- src/components/ActionCard/CommentMobile.jsx
+++ src/components/ActionCard/CommentMobile.jsx
@@ -8,20 +8,25 @@ import PropTypes from 'prop-types'
 import _ from 'lodash'
 import moment from 'moment'
 import { Link } from 'react-router-dom'
+import cn from 'classnames'
+
 import UserWithName from '../User/UserWithName'
 import { POST_TIME_FORMAT } from '../../config/constants.js'
 
 import './CommentMobile.scss'
 
-const CommentMobile = ({ messageId, author, date, children }) => {
+const CommentMobile = ({ messageId, author, date, children, noInfo }) => {
   const messageLink = window.location.pathname.substr(0, window.location.pathname.indexOf('#')) + `#comment-${messageId}`
 
   return (
-    <div styleName="comment" id={`comment-${messageId}`}>
-      <div styleName="header">
-        <UserWithName {..._.pick(author, 'firstName', 'lastName', 'photoURL')} size="40" />
-        <Link styleName="date" to={messageLink}>{moment(date).format(POST_TIME_FORMAT)}</Link>
-      </div>
+    <div styleName={cn('comment', {'is-bundled' : noInfo})} id={`comment-${messageId}`}>
+      {
+        !noInfo &&
+        <div styleName="header">
+          <UserWithName {..._.pick(author, 'firstName', 'lastName', 'photoURL')} size="40" />
+          <Link styleName="date" to={messageLink}>{moment(date).format(POST_TIME_FORMAT)}</Link>
+        </div>
+      }
       <div styleName="text" className="draftjs-post">{children}</div>
     </div>
   )
@@ -31,7 +36,8 @@ CommentMobile.propTypes = {
   messageId: PropTypes.string.isRequired,
   author: PropTypes.any.isRequired,
   date: PropTypes.any.isRequired,
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  noInfo: PropTypes.bool
 }
 
 export default CommentMobile
