1c9e23987d2d8b07e2188a1b811fa08d20896ac1
RishiRaj <rishiraj.sahu@topcoder.com>
2018-06-27 15:44:43 +0530

    Fixed#2097 Show all active phases on dashboard

--- src/projects/detail/components/WorkInProgress/WorkInProgress.jsx
+++ src/projects/detail/components/WorkInProgress/WorkInProgress.jsx
@@ -11,14 +11,39 @@ import ProjectStage from '../ProjectStage'
 
 import './WorkInProgress.scss'
 
-const WorkInProgress = (props) => (
-  <Section>
-    <SectionTitle title="Work in progress">
-      <Link to={`/projects/${props.project.id}/plan`} styleName="view-all">View all</Link>
-    </SectionTitle>
-    <ProjectStage {...props} />
-  </Section>
-)
+const WorkInProgress = ({
+  productTemplates, currentMemberRole, isProcessing, isSuperUser, isManageUser, project, activePhases, updateProduct,
+  fireProductDirty, fireProductDirtyUndo, addProductAttachment, updateProductAttachment, removeProductAttachment
+}) => {
+  const renderActivePhases = (activePhase, index) => {
+    return (
+      <ProjectStage
+        key={index}
+        productTemplates={productTemplates}
+        currentMemberRole={currentMemberRole}
+        isProcessing={isProcessing}
+        isSuperUser={isSuperUser}
+        isManageUser={isManageUser}
+        project={project}
+        phase={activePhase}
+        updateProduct={updateProduct}
+        fireProductDirty={fireProductDirty}
+        fireProductDirtyUndo={fireProductDirtyUndo}
+        addProductAttachment={addProductAttachment}
+        updateProductAttachment={updateProductAttachment}
+        removeProductAttachment={removeProductAttachment}
+      />
+    )
+  }
+  return (
+    <Section>
+      <SectionTitle title="Work in progress">
+        <Link to={`/projects/${project.id}/plan`} styleName="view-all">View all</Link>
+      </SectionTitle>
+      {activePhases.map(renderActivePhases)}
+    </Section>
+  )
+}
 
 WorkInProgress.propTypes = {
   project: PT.object.isRequired,
