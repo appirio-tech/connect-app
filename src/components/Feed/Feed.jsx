c2d068e4db7c7d247f201e121b5b9a46e061ac77
Samir Gondzetovic <gondzo@gmail.com>
2018-07-27 15:28:30 +0100

    disable deleting the first comment

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -131,7 +131,7 @@ class Feed extends React.Component {
                   {self && !isFullScreen && (
                     <CommentEditToggle
                       forTopic
-                      hideDelete={comments.length < 2}
+                      hideDelete={comments.length > 1}
                       onEdit={this.onEditTopic}
                       onDelete={onDeleteTopic}
                     />
