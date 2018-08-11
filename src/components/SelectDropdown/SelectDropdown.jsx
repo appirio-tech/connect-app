63bf44df42ba7cc967f029b1f64d5ff7011ba08e
Maksym Mykhailenko <maxceem@gmail.com>
2018-08-11 10:25:24 +0800

    issue #2298 - replaced browser confirmation dialog with the modal popup

--- src/components/SelectDropdown/SelectDropdown.jsx
+++ src/components/SelectDropdown/SelectDropdown.jsx
@@ -4,13 +4,21 @@ import cn from 'classnames'
 import PT from 'prop-types'
 import { HOC as hoc } from 'formsy-react'
 import Dropdown from 'appirio-tech-react-components/components/Dropdown/Dropdown'
+import Modal from 'react-modal'
 
 import './SelectDropdown.scss'
 
 class SelectDropdown extends Component {
   constructor(props) {
     super(props)
-    this.state = _.assign(this.state, { selectedOption : null })
+
+    this.state = {
+      selectedOption: null,
+      confirmOption: null,
+    }
+
+    this.cancelSelectOption = this.cancelSelectOption.bind(this)
+    this.confirmSelectOption = this.confirmSelectOption.bind(this)
   }
 
   componentWillMount() {
@@ -43,22 +51,44 @@ class SelectDropdown extends Component {
   }
 
   handleClick(option) {
-    if (
-      !option.confirm ||
-      option.confirm && window.confirm(option.confirm)
-    ) {
-      this.setState({ selectedOption : option }, function() {
-        if (this.props.onSelect && typeof this.props.onSelect === 'function')  {
-          this.props.onSelect(this.state.selectedOption)
-        }
-      })
-      this.props.setValue(option.value)
+    if (!option.confirm) {
+      this.selectOption(option)
+    } else {
+      this.showConfirmOption(option)
     }
+  }
+
+  showConfirmOption(option) {
+    this.setState({
+      confirmOption: option,
+    })
+  }
+
+  cancelSelectOption() {
+    this.setState({
+      confirmOption: null,
+    })
+  }
+
+  confirmSelectOption() {
+    this.selectOption(this.state.confirmOption)
+    this.setState({
+      confirmOption: null,
+    })
+  }
+
+  selectOption(option) {
+    this.setState({ selectedOption : option }, function() {
+      if (this.props.onSelect && typeof this.props.onSelect === 'function')  {
+        this.props.onSelect(this.state.selectedOption)
+      }
+    })
+    this.props.setValue(option.value)
   }
 
   render() {
     const { options, theme, disabled } = this.props
-    const { selectedOption } = this.state
+    const { selectedOption, confirmOption } = this.state
     const selectedValue = selectedOption.title
 
     const renderOption = (option, optIdx) => {
@@ -80,18 +110,37 @@ class SelectDropdown extends Component {
       )
     }
     return (
-      disabled ? (
-        <div className="SelectDropdown" styleName="dropdown-disabled">
-          <div className="dropdown-menu-header"><span className="tc-link">{ selectedValue }</span></div>
-        </div>
-      ) : (
-        <Dropdown theme={ theme } className="SelectDropdown" noPointer>
-          <div className="dropdown-menu-header"><span className="tc-link">{ selectedValue }</span></div>
-          <ul className="dropdown-menu-list">
-            { options.map(renderOption) }
-          </ul>
-        </Dropdown>
-      )
+      <div styleName="container">
+        {disabled ? (
+          <div className="SelectDropdown" styleName="dropdown-disabled">
+            <div className="dropdown-menu-header"><span className="tc-link">{ selectedValue }</span></div>
+          </div>
+        ) : (
+          <Dropdown theme={ theme } className="SelectDropdown" noPointer>
+            <div className="dropdown-menu-header"><span className="tc-link">{ selectedValue }</span></div>
+            <ul className="dropdown-menu-list">
+              { options.map(renderOption) }
+            </ul>
+          </Dropdown>
+        )}
+
+        <Modal
+          isOpen={!!confirmOption}
+          className="delete-post-dialog"
+          overlayClassName="delete-post-dialog-overlay"
+          onRequestClose={this.cancelSelectOption}
+          contentLabel=""
+        >
+          <div className="modal-title">
+            {!!confirmOption && confirmOption.confirm}
+          </div>
+
+          <div className="button-area flex center action-area">
+            <button className="tc-btn tc-btn-default tc-btn-sm action-btn btn-cancel" onClick={this.cancelSelectOption}>Cancel</button>
+            <button className="tc-btn tc-btn-warning tc-btn-sm action-btn " onClick={this.confirmSelectOption}>Confirm</button>
+          </div>
+        </Modal>
+      </div>
     )
   }
 }
