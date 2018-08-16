a23ee14e53d400bf9e139b342cdfb9c8b9a7f09d
Vikas Agarwal <vikas.agarwal@topcoder.com>
2018-08-16 15:05:13 +0530

    Added missing variables

--- config/constants/master.js
+++ config/constants/master.js
@@ -45,5 +45,6 @@ module.exports = {
 
   TC_NOTIFICATION_URL: 'https://api.topcoder.com/v5/notifications',
   CONNECT_MESSAGE_API_URL: 'https://api.topcoder.com/v5',
-  TC_SYSTEM_USERID: process.env.PROD_TC_SYSTEM_USERID
+  TC_SYSTEM_USERID: process.env.PROD_TC_SYSTEM_USERID,
+  MAINTENANCE_MODE: process.env.PROD_MAINTENANCE_MODE
 }
