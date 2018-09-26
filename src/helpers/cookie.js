e7b8eb4b69b249ef2b10f94a2c498ec19bc7627f
Maksym Mykhailenko <maxceem@gmail.com>
2018-09-26 10:11:12 +0800

    issue #2522 - fix lint errors, removed not used svg images

--- src/helpers/cookie.js
+++ src/helpers/cookie.js
@@ -1,4 +1,4 @@
 export function getCookie(name) {
-  const v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-  return v ? v[2] : undefined;
+  const v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)')
+  return v ? v[2] : undefined
 }
