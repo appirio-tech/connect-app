3f7c2f9ba911fd2727b4a859671596ce1361ce54
RishiRaj <rishiraj.sahu@topcoder.com>
2018-09-27 11:02:00 +0530

    Git-2526:Better position for Notification settings link

--- src/components/NotificationsDropdownHeader/NotificationsDropdownHeader.jsx
+++ src/components/NotificationsDropdownHeader/NotificationsDropdownHeader.jsx
@@ -5,12 +5,19 @@
  */
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import './NotificationsDropdownHeader.scss'
 
 const NotificationsDropdownHeader = (props) => (
   <div className="notifications-dropdown-header">
     <h3 className="header">Notifications</h3>
-    <button className="tc-btn tc-btn-link mark-all" onClick={props.onMarkAllClick} disabled={!props.hasUnread}>Mark all as read</button>
+    <div className="right-content">
+      <button className="tc-btn tc-btn-link mark-all" onClick={props.onMarkAllClick} disabled={!props.hasUnread}>Mark all as read</button>
+      {
+        props.hasUnread ?
+          (<div className="right-content"><span className="dot"/><Link to="/settings/notifications" className="tc-btn tc-btn-link settings">Settings</Link></div>) : ''
+      }
+    </div>
   </div>
 )
 
