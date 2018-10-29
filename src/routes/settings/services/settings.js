b6c998fb8614b7a6d4fa51f838b7dce23880a365
Maksym Mykhailenko <maxceem@gmail.com>
2018-10-29 08:51:59 +0800

    issue #2654 - added some comment docs

--- src/routes/settings/services/settings.js
+++ src/routes/settings/services/settings.js
@@ -4,11 +4,23 @@
 import { axiosInstance as axios } from '../../../api/requestInterceptor'
 import { TC_NOTIFICATION_URL } from '../../../config/constants'
 
+/**
+ * Get notifications settings
+ * 
+ * @returns {Promise<Object>} notifications settings
+ */
 const getNotificationSettings = () => {
   return axios.get(`${TC_NOTIFICATION_URL}/settings`)
     .then(resp => resp.data)
 }
 
+/**
+ * Save notifications settings
+ * 
+ * @param {Object} notifications settings
+ * 
+ * @returns {Promise<Object>} notifications settings
+ */
 const saveNotificationSettings = (data) => {
   return axios.put(`${TC_NOTIFICATION_URL}/settings`, data)
 }
