32f6a9aecabbf799647cd8af5937d99a8b341712
Maksym Mykhailenko <maxceem@gmail.com>
2018-10-30 13:07:28 +0800

    issue #2654 - verification email container with success/failure/expired messages

--- src/routes/settings/routes.jsx
+++ src/routes/settings/routes.jsx
@@ -9,9 +9,15 @@ import NotificationSettingsContainer from './routes/notifications/containers/Not
 import SettingsToolBar from './components/SettingsToolBar'
 import SystemSettingsContainer from './routes/system/containers/SystemSettingsContainer'
 import ProfileSettingsContainer from './routes/profile/containers/ProfileSettingsContainer'
+import EmailVerificationFailure from './routes/email-verification/components/Failure'
+import EmailVerificationSuccessContainer from './routes/email-verification/containers/SuccessContainer'
 
 export default [
-  <Route key={'system'} path="/settings/account" render={renderApp(<TopBarContainer toolbar={SettingsToolBar} />, <SystemSettingsContainer />)} />,
-  <Route key={'notifications'} path="/settings/notifications" render={renderApp(<TopBarContainer toolbar={SettingsToolBar} />, <NotificationSettingsContainer />)} />,
-  <Route key={'profile'} path="/settings/profile" render={renderApp(<TopBarContainer toolbar={SettingsToolBar} />, <ProfileSettingsContainer />)} />
+  <Route key="account" exact path="/settings/account" render={renderApp(<TopBarContainer toolbar={SettingsToolBar} />, <SystemSettingsContainer />)} />,
+  <Route key="account/email-verification/success" exact path="/settings/account/email-verification/success/:handle/:token/:newEmail/:oldEmail/:jwtToken" render={renderApp(<TopBarContainer toolbar={SettingsToolBar} />, <EmailVerificationSuccessContainer />)} />,
+  <Route key="account/email-verification/failure" exact path="/settings/account/email-verification/failure" render={renderApp(<TopBarContainer toolbar={SettingsToolBar} />, <EmailVerificationFailure />)} />,
+  
+  <Route key="notifications" exact path="/settings/notifications" render={renderApp(<TopBarContainer toolbar={SettingsToolBar} />, <NotificationSettingsContainer />)} />,
+  <Route key="profile" exact path="/settings/profile" render={renderApp(<TopBarContainer toolbar={SettingsToolBar} />, <ProfileSettingsContainer />)} />,
+  
 ]
