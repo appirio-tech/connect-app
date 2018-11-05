c2e854af265c3f8e0d92639392c31cdf9a25cdb5
Maksym Mykhailenko <maxceem@gmail.com>
2018-11-05 20:00:26 +0800

    lin fix

--- src/routes/settings/routes/profile/components/ProfileSettingsForm.jsx
+++ src/routes/settings/routes/profile/components/ProfileSettingsForm.jsx
@@ -40,7 +40,7 @@ class ProfileSettingsForm extends Component {
     let validations = null
     if (name === 'businessPhone') {
       validations = {
-        matchRegexp: /^([+]\d{1,3}[\.\-\s]?)(\d{2,4}[\.\-\s]?){2,3}$/
+        matchRegexp: /^(\+\d{1,3}[.\-\s]?)(\d{2,4}[.\-\s]?){3}$/
       }
     } else if (name === 'firstNLastName') {
       validations = {
