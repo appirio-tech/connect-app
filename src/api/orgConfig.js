b8c5adf9f93cd1fc490406b5b70f99a67dfa2fea
Vikas Agarwal <vikas.agarwal@topcoder.com>
2019-01-30 15:39:52 +0530

    Updated path for the orgConfig endpoint

--- src/api/orgConfig.js
+++ src/api/orgConfig.js
@@ -10,7 +10,7 @@ import { PROJECTS_API_URL } from '../config/constants'
  * @returns {Promise<Object>} organization config data
  */
 export function getOrgConfig(groupIds) {
-  return axios.get(`${PROJECTS_API_URL}/v4/orgConfig?filter=orgId=in(${groupIds})`)
+  return axios.get(`${PROJECTS_API_URL}/v4/projects/metadata/orgConfig?filter=orgId=in(${groupIds})`)
     .then(resp => {
       return _.get(resp.data, 'result.content', {})
     })
