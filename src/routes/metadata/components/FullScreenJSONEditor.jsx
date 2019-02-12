3b65e33f8184273e392f2f2b9567f54b7ced8241
Vikas Agarwal <vikas.agarwal@topcoder.com>
2019-02-12 14:59:48 +0530

    Removed conflicting library for time being. It is causing issues with IE because of ES6 syntax.

--- src/routes/metadata/components/FullScreenJSONEditor.jsx
+++ src/routes/metadata/components/FullScreenJSONEditor.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import JSONInput from 'react-json-editor-ajrm'
-import locale    from 'react-json-editor-ajrm/locale/en'
+// import JSONInput from 'react-json-editor-ajrm'
+// import locale    from 'react-json-editor-ajrm/locale/en'
 
 import MobilePage from '../../../components/MobilePage/MobilePage'
 import './FullScreenJSONEditor.scss'
 
 const FullScreenJSONEditor = ({
-  onJSONEdit,
+  // onJSONEdit,
   onExit,
-  json
+  // json
 }) => (
   <MobilePage keepToolbar>
     <div styleName="full-screen-json-editor">
       <button type="button" className="tc-btn tc-btn-primary tc-btn-sm" onClick={onExit}>Restore</button>
-      <JSONInput
+      {/* <JSONInput
         id="jsonField"
         placeholder ={ json }
         theme="dark_vscode_tribute"
@@ -22,7 +22,7 @@ const FullScreenJSONEditor = ({
         width="100%"
         onChange={onJSONEdit}
         // onKeyPressUpdate={false}
-        waitAfterKeyPress={3000}
+        waitAfterKeyPress={3000} */}
       />
     </div>
   </MobilePage>
