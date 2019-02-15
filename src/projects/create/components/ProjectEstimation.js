5c05793e3214cf327789c6a90abc9b2e255225a0
Maksym Mykhailenko <maxceem@gmail.com>
2019-02-15 10:40:49 +0800

    fix duration sum calculation, improved readability

--- src/projects/create/components/ProjectEstimation.js
+++ src/projects/create/components/ProjectEstimation.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import _ from 'lodash'
 import PT from 'prop-types'
 import { flatten } from 'flat'
+import cn from 'classnames'
 
 import { evaluate } from '../../../helpers/dependentQuestionsHelper'
 import { getProductEstimate } from '../../../config/projectWizard'
@@ -10,25 +12,31 @@ import './ProjectEstimation.scss'
 const numberWithCommas = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 
 function ProjectEstimation({ question, project, projectTemplate }) {
-  const totalDuration = question.deliverables.reduce((p, c) => p+c.duration, 0)
   const isSelected = (item) => evaluate(item.enableCondition, flatten(project, { safe: true }))
   const phasesEnabled = question.deliverables.filter(isSelected)
+  const totalDuration = _.sumBy(phasesEnabled, 'duration')
 
   const { priceEstimate } = getProductEstimate({scope: projectTemplate}, project)
 
   return (
     <div styleName="ProjectEstimation">
       <div styleName="title">
-        <h5>
-          {question.title}
-        </h5>
+        <h5>{question.title}</h5>
         <span>{phasesEnabled.length ? (
-          phasesEnabled.length +' phases, ' + totalDuration + ' weeks'
-        ) : 'No phase selected'}</span>
+          phasesEnabled.length + ' phases, ' + totalDuration + ' weeks'
+        ) : (
+          'No phase selected'
+        )}</span>
       </div>
       <ul styleName="project-estimate-timeline">
-        {question.deliverables.map((item, i) => (
-          <li key={i} style={{width: (item.duration / totalDuration * 100) + '%'}} className={(isSelected(item) ? 'selected' : '') + (' type-'+item.id)}>
+        {question.deliverables.map(item => (
+          <li
+            key={item.id}
+            style={{
+              width: (item.duration / totalDuration * 100) + '%'
+            }}
+            className={cn(`type-${item.id}`, { selected: isSelected(item) })}
+          >
             <div styleName="item-title">{item.title}</div>
             <span styleName="item-duration">{item.duration}W</span>
           </li>
