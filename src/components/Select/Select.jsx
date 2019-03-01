ab29f108e0a8c4b7e57784c0e7423ed0b70d993b
Gaurav Gautam <gautam1168@gmail.com>
2019-03-01 11:01:41 +0530

    Styling changes for autoselect input

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -9,11 +9,34 @@
 import React from 'react'
 import ReactSelect from 'react-select'
 // import '../../styles/react-select.css'
-import './Select.scss'
+import stylevars from './Select.scss'
+
+const customStyles = {
+	control: (provided, state) => ({
+		...provided,
+		'border-color': '#aaaaab',
+		'& > div': { height: '40px' }
+	}),
+	option: (provided, state) => ({
+		...provided,
+		'font-family': 'Roboto, Arial, Helvetica, sans-serif',
+		'font-size': '12px'
+	}),
+	multiValue: (provided, state) => ({
+		...provided,
+		'background-color': '#cdcdce',
+		'font-weight': 'bold'
+	}),
+	dropdownIndicator: (provided, state) => ({
+		...provided,
+		display: 'none'
+	})
+}
 
 const Select = (props) => {
+  console.log('Select class: ', stylevars)
   return (
-    <ReactSelect {...props} />
+    <ReactSelect {...props} styles={customStyles}/>
   )
 }
 
