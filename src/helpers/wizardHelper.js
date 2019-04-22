ff5cf79f005bdfbd6abbc6f8a4c7dc41d557434d
Maksym Mykhailenko <maxceem@gmail.com>
2019-04-22 16:40:49 +0800

    fix skip add-ons when there are no add-ons sections or no intermediate section

--- src/helpers/wizardHelper.js
+++ src/helpers/wizardHelper.js
@@ -945,42 +945,45 @@ export const updateNodesByConditions = (template, project, productTemplates) => 
   //      in the future we should re-implement it without using id to support it more general case if possible
   const summaryIntermediateSection = updatedTemplate.sections.find(section => section.id === 'summary-intermediate')
   const addOnSection =  updatedTemplate.sections.find(section => section.id === 'add-ons')
-  let hasActiveAddOnSection = false
-  const hideByConditionUpdateRule = {
-    __wizard: {
-      hiddenByCondition: { $set: true }
-    }
-  }
-  const showByConditionUpdateRule = {
-    __wizard: {
-      hiddenByCondition: { $set: false }
-    }
-  }
-  
-
-  addOnSection.subSections.forEach(subSection => {
-    subSection.questions.forEach(q => {
-      // skip add-ons step if no product templates found
-      const doesNotHaveAddOns = !_.some(productTemplates, { category: q.category, isAddOn: true })
-      if (q.type === 'add-ons' && doesNotHaveAddOns) {
-        updatedTemplate = updateNodeObject(updatedTemplate, q.__wizard.node, hideByConditionUpdateRule)
+  // only add this logic when template has both 'summary-intermediate' and 'add-ons' sections
+  if (summaryIntermediateSection && addOnSection) {
+    let hasActiveAddOnSection = false
+    const hideByConditionUpdateRule = {
+      __wizard: {
+        hiddenByCondition: { $set: true }
       }
-
-      // as we updated template without mutation, we should check get the updated value of the question from the updated template
-      const updatedQuestionNodeObject = getNodeObject(updatedTemplate, q.__wizard.node)
-      if (!_.get(updatedQuestionNodeObject, '__wizard.hiddenByCondition')) {
-        hasActiveAddOnSection = true
+    }
+    const showByConditionUpdateRule = {
+      __wizard: {
+        hiddenByCondition: { $set: false }
       }
+    }
+    
+
+    addOnSection.subSections.forEach(subSection => {
+      subSection.questions.forEach(q => {
+        // skip add-ons step if no product templates found
+        const doesNotHaveAddOns = !_.some(productTemplates, { category: q.category, isAddOn: true })
+        if (q.type === 'add-ons' && doesNotHaveAddOns) {
+          updatedTemplate = updateNodeObject(updatedTemplate, q.__wizard.node, hideByConditionUpdateRule)
+        }
+
+        // as we updated template without mutation, we should check get the updated value of the question from the updated template
+        const updatedQuestionNodeObject = getNodeObject(updatedTemplate, q.__wizard.node)
+        if (!_.get(updatedQuestionNodeObject, '__wizard.hiddenByCondition')) {
+          hasActiveAddOnSection = true
+        }
+      })
     })
-  })
 
-  // skip intermediate summary step if no add-ons step found
-  // NOTE  At the moment we can theoretically have other questions in the add-ons section so we don't hide it explicitly here. 
-  //       But as at the moment add-ons section has only add-ons questions, it would be hided automatically as a section without any visible question.
-  if (!hasActiveAddOnSection) {
-    updatedTemplate = updateNodeObject(updatedTemplate, summaryIntermediateSection.__wizard.node, hideByConditionUpdateRule)
-  } else {
-    updatedTemplate = updateNodeObject(updatedTemplate, summaryIntermediateSection.__wizard.node, showByConditionUpdateRule)
+    // skip intermediate summary step if no add-ons step found
+    // NOTE  At the moment we can theoretically have other questions in the add-ons section so we don't hide it explicitly here. 
+    //       But as at the moment add-ons section has only add-ons questions, it would be hided automatically as a section without any visible question.
+    if (!hasActiveAddOnSection) {
+      updatedTemplate = updateNodeObject(updatedTemplate, summaryIntermediateSection.__wizard.node, hideByConditionUpdateRule)
+    } else {
+      updatedTemplate = updateNodeObject(updatedTemplate, summaryIntermediateSection.__wizard.node, showByConditionUpdateRule)
+    }
   }
 
   return {
