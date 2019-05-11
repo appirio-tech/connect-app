bd088354466c9e483948f6b908aae68c532aa0af
prakashdurlabhji <prakashseta@gmail.com>
2019-05-12 04:09:31 +0530

    issue 2991 fix

--- src/components/CoderBot/CoderBot.jsx
+++ src/components/CoderBot/CoderBot.jsx
@@ -18,13 +18,32 @@ const getHeading = code => {
   }
 }
 
-const getMessage = code => {
+const getMessage = (code, heading, message) => {
+  const subject = "Topcoder Connect Issue Report";
+  let body = heading || getHeading(code);
+  if (message) {
+    body = `${body} <br> ${message}`;
+  }
   switch(code) {
   case 200:
     return 'Operation performed successfully!!'
   default:
-    return 'Sorry about that, mate! Please try reloading the page again. If things don’t work or you’re sure it is Coder’s fault, send us a note at <a href="support@topcoder.com">support@topcoder.com</a> and we’ll fix it for you.'
+    return `Sorry about that, mate! Please try reloading the page again. If things don’t work or you’re sure it is Coder’s fault, send us a note at <a href="mailto:support@topcoder.com?subject=${subject}&body=${body}">support@topcoder.com</a> and we’ll fix it for you.`
   }
+}
+
+const copyToClipboard = (code, heading, message) => {
+  const subject = "Topcoder Connect Issue Report";
+  let body = heading || getHeading(code);
+  if (message) {
+    body = `${body} <br> ${message}`;
+  }
+  const textField = document.createElement('textarea')
+  textField.innerText = `${subject} ${body}`
+  document.body.appendChild(textField)
+  textField.select()
+  document.execCommand('copy')
+  textField.remove()
 }
 
 const CoderBot = ({code, message, heading, children}) => {
@@ -34,8 +53,16 @@ const CoderBot = ({code, message, heading, children}) => {
         <div className="page-error">
           <h3>{ heading || getHeading(code) }</h3>
           <div className="content">
-            <p dangerouslySetInnerHTML={ {__html : message || getMessage(code) } } />
+            <p dangerouslySetInnerHTML={ {__html : message || getMessage(code, heading) } } />
             <div>{children}</div>
+          </div>
+          <div>
+            <button
+              onClick= { () => copyToClipboard(code, heading, message) }
+              className="tc-btn tc-btn-primary"
+            >
+              Copy error details
+            </button>
           </div>
           {code !== 200 ? <CoderBroken className="icon-coder-broken" /> : <CoderHappy className="icon-coder-broken" />}
           <span>{code !== 200 && code}</span>
