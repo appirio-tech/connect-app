5090e3f2883e65c5147c97d59aecacb7f9118674
rashmi73 <rashmiseta73@gmail.com>
2019-05-12 22:58:49 +0530

    issue 2936 fix

---
+++ src/components/NotificationBadge/NotificationBadge.jsx
@@ -0,0 +1,41 @@
+/**
+ * Notification Badge
+ */
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Tooltip from 'appirio-tech-react-components/components/Tooltip/Tooltip'
+import { TOOLTIP_DEFAULT_DELAY } from '../../../config/constants'
+
+import './NotificationBadge.scss'
+
+const getBadge = (number) => {
+    return (<div styleName="badge">{number}</div>)
+}
+
+const NotificationBadge = ({count, text}) => {
+    if (text) {
+        return (
+            <Tooltip theme="light" tooltipDelay={TOOLTIP_DEFAULT_DELAY}>
+                <div className="tooltip-target">
+                    {getBadge(count)}
+                </div>
+                <div className="tooltip-body">
+                    {text}
+                </div>
+            </Tooltip>
+        )
+    } else {
+        return (
+            getBadge(count)
+        )
+    }
+}
+
+NotificationBadge.propTypes = {
+   count: PropTypes.number.isRequired,
+   text: PropTypes.string,
+   id: PropTypes.number
+}
+export default NotificationBadge
+  
