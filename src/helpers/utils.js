b27c424c6f8693728c97fe0d67020d5d446e2f7f
Maksym Mykhailenko <maxceem@gmail.com>
2019-06-04 17:06:27 +0800

    when validating if user has been already invited by the email, check emails case-insensetive way

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -20,3 +20,27 @@ export function difference(object, base) {
   }
   return changes(object, base)
 }
+
+/**
+ * Helper method to check the uniqueness of two emails
+ *
+ * @param {String} email1    first email to compare
+ * @param {String} email2    second email to compare
+ * @param {Object} options  the options
+ *
+ * @returns {Boolean} true if two emails are same
+ */
+export const compareEmail = (email1, email2, options = { UNIQUE_GMAIL_VALIDATION: false }) => {
+  if (options.UNIQUE_GMAIL_VALIDATION) {
+    // email is gmail
+    const emailSplit = /(^[\w.+-]+)(@gmail\.com|@googlemail\.com)$/g.exec(_.toLower(email1))
+    if (emailSplit) {
+      const address = emailSplit[1].replace('.', '')
+      const emailDomain = emailSplit[2].replace('.', '\\.')
+      const regexAddress = address.split('').join('\\.?')
+      const regex = new RegExp(`${regexAddress}${emailDomain}`)
+      return regex.test(_.toLower(email2))
+    }
+  }
+  return _.toLower(email1) === _.toLower(email2)
+}
