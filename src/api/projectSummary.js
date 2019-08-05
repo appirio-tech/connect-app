7f55078b9cceef2fc80fd0ae6e1a887eaeaf6c81
Mihai Cucicea <mishacucicea@yahoo.com>
2019-08-05 17:03:22 +0300

    Initial Reports API integration.

--- src/api/projectSummary.js
+++ src/api/projectSummary.js
@@ -1,25 +1,36 @@
+import _ from 'lodash'
+import { axiosInstance as axios } from './requestInterceptor'
+import { PROJECTS_API_URL } from '../config/constants'
+
 /**
  * Get a project summary
  *
  * @param  {integer} projectId unique identifier of the project
  */
 export function getProjectSummary(projectId) {
-  return Promise.resolve({
-    projectId,
-    budget: {
-      work: 40250,
-      fees: 10430,
-      revenue: 40250,
-      remaining: 19750
-    },
-    topcoderDifference: {
-      countries: 12,
-      registrants: 165,
-      designs: 38,
-      linesOfCode: 1500000,
-      hoursSaved: 450,
-      costSavings: 45000,
-      valueCreated: 670000
-    }
-  })
+  return axios.get(`${PROJECTS_API_URL}/v4/projects/${projectId}/reports?reportName=summary`)
+    .then(resp => {
+      const res = _.get(resp.data, 'result.content', {})
+      const challengeMetrics = _.find(res, c => c['challenge.track'] === null)
+
+      return Promise.resolve({
+        projectId,
+        budget: {
+          work: 40250,
+          fees: 10430,
+          revenue: 40250,
+          remaining: 19750
+        },
+        topcoderDifference: {
+          countries: 12,
+          registrants: challengeMetrics['challenge.num_registrations'],
+          designs: challengeMetrics['challenge.num_submissions'],
+          linesOfCode: 1500000,
+          hoursSaved: 450,
+          costSavings: 45000,
+          valueCreated: 670000
+        }
+      })
+    })
+
 }
