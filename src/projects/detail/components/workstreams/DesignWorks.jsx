5fb86ff2a81758ed58742c5a7a2ee5da657b3990
Felix Kwan <kwfxng@gmail.com>
2019-08-12 22:29:51 +0800

    add DesignWorksContainer initial styling and layout

--- src/projects/detail/components/workstreams/DesignWorks.jsx
+++ src/projects/detail/components/workstreams/DesignWorks.jsx
@@ -5,6 +5,8 @@ import React from 'react'
 import PT from 'prop-types'
 import { withRouter } from 'react-router-dom'
 import BackIcon from '../../../../assets/icons/arrows-16px-1_tail-left.svg'
+import CloseIcon from '../../../../assets/icons/x-mark-black.svg'
+import './DesignWork.scss'
 
 class DesignWorks extends React.Component {
   constructor(props) {
@@ -12,14 +14,40 @@ class DesignWorks extends React.Component {
     this.state = {}
   }
 
+  /**
+   * Get url of dashboard
+   */
+  getDashboardUrl() {
+    const { match } = this.props
+    return `/projects/${match.params.projectId}`
+  }
+
   render() {
     const { onBack } = this.props
     return (
-      <div>
-        Testing DesignWorks
-        <div onClick={onBack} className="left-control">
-          <i className="icon" title="back"><BackIcon /></i>
-          <span className="back-icon-text">Back</span>
+      <div styleName="container">
+        <div styleName="header">
+          <div onClick={onBack} styleName="left-control">
+            <i styleName="icon" title="back"><BackIcon /></i>
+            <span styleName="back-icon-text">Back</span>
+          </div>
+          <div styleName="title">ADD DESIGN LINKS</div>
+          <div styleName="right-control">
+            <i onClick={() => {
+              this.props.history.push(this.getDashboardUrl())
+              onBack()
+            }} styleName="icon-close"
+            >
+              <CloseIcon />
+            </i>
+          </div>
+        </div>
+        <div styleName="complete-wrapper">
+          <div styleName="complete-message">Add all the designs for the review. After all the designs are added, click the "Complete" button, so customer may start reviewing them.</div>
+          <button styleName="complete-btn" className="tc-btn tc-btn-warning tc-btn-sm action-btn " onClick={() => {}}>Complete</button>
+        </div>
+        <div styleName="add-design-option-wrapper">
+          <button styleName="add-design-option-btn" className="tc-btn tc-btn-primary tc-btn-sm action-btn " onClick={() => {}}>Add design option</button>
         </div>
       </div>
     )
