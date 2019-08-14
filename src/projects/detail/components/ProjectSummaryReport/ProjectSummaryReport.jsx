2b7d9064850ed74a7be08f0521858cb5f0e2c8e3
Vikas Agarwal <vikas.agarwal@topcoder.com>
2019-08-14 15:36:39 +0530

    Removed projectestimation component for now. We need to build and include the proper work progress component later.

--- src/projects/detail/components/ProjectSummaryReport/ProjectSummaryReport.jsx
+++ src/projects/detail/components/ProjectSummaryReport/ProjectSummaryReport.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import PT from 'prop-types'
-import ProjectEstimation from '../../../create/components/ProjectEstimation'
 import ProjectBudgetReport from './ProjectBudgetReport'
 import TopcoderDifferenceReport  from './TopcoderDifferenceReport'
 import PERMISSIONS from '../../../../config/permissions'
@@ -8,7 +7,7 @@ import { checkPermission } from '../../../../helpers/permissions'
 
 import './ProjectSummaryReport.scss'
 
-const ProjectSummaryReport = ({ projectSummary, project, template, estimationQuestion }) => {
+const ProjectSummaryReport = ({ projectSummary, project }) => {
   const { work, fees, revenue, remaining } = projectSummary.budget
   const hasBudgetPermission = checkPermission(PERMISSIONS.ACCESS_BUDGET_REPORT, project)
   const showBudgetSummary = hasBudgetPermission && (work || fees || revenue || remaining)
@@ -17,18 +16,6 @@ const ProjectSummaryReport = ({ projectSummary, project, template, estimationQue
   const showEmptyProjectSummary = !showBudgetSummary && !showTopcoderDifference
   return (
     <div styleName="wrapper">
-      {!!estimationQuestion &&
-        <div styleName="project-estimation">
-          <ProjectEstimation
-            onClick={() => {}}
-            question={estimationQuestion}
-            template={template}
-            project={project}
-            theme="dashboard"
-            showPrice={false}
-          />
-        </div>
-      }
       { showEmptyProjectSummary &&
         <div>Sorry, we don't have any reports available for the project as of now. Please contact the account executive for any details.</div>
       }
