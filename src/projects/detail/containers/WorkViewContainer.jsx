b0a7023598a4b0b6fcdeb31b444f1df0bfcf1d9e
Maksym Mykhailenko <maxceem@gmail.com>
2019-08-21 13:54:45 +0800

    feat: start loading all comments for work when we open work

--- src/projects/detail/containers/WorkViewContainer.jsx
+++ src/projects/detail/containers/WorkViewContainer.jsx
@@ -6,10 +6,13 @@
  */
 import React from 'react'
 import PT from 'prop-types'
+import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
 import spinnerWhileLoading from '../../../components/LoadingSpinner'
 import WorkView from '../components/workstreams/WorkView'
+
+import { loadTopic } from '../../../actions/topics'
 
 
 // This handles showing a spinner while the state is being loaded async
@@ -23,10 +26,23 @@ class WorkViewContainer extends React.Component {
   }
 
   componentWillMount() {
-    const { match: { params: { projectId, workstreamId, workId } }, loadWorkInfo, work } = this.props
+    const {
+      match: { params: { projectId, workstreamId, workId } },
+      loadWorkInfo,
+      work,
+      loadTopic,
+      topics,
+    } = this.props
     if (!work || `${work.id}` !== workId) {
       loadWorkInfo(projectId, workstreamId, workId)
     }
+
+    // start loading all the comments for work if not yet loaded or started loading
+    [`work#${workId}-details`, `work#${workId}-requirements`].forEach((topicTag) => {
+      if (!topics[topicTag]) {
+        loadTopic(projectId, topicTag)
+      }
+    })
   }
 
   render() {
@@ -46,6 +62,16 @@ WorkViewContainer.PropTypes = {
   inputDesignWorks: PT.func.isRequired,
   startDesignReview: PT.func.isRequired,
   markMilestoneAsCompleted: PT.func,
+  topics: PT.array,
+  loadTopic: PT.func.isRequired,
 }
 
-export default withRouter(WorkViewContainer)
+const mapStateToProps = ({ topics }) => ({
+  topics,
+})
+
+const mapDispatchToProps = {
+  loadTopic,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(WorkViewContainer))
