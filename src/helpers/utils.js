b6dd9457565ba163d6d7e0b01934895d3d73c085
rashmi73 <41687423+rashmi73@users.noreply.github.com>
2019-09-24 19:59:28 +0530

    Update utils.js

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -53,9 +53,12 @@ export const compareEmail = (email1, email2, options = { UNIQUE_GMAIL_VALIDATION
  *
  * @returns {Boolean} true if two user handles are same
  */
-export const compareHandles = (handle1, handle2) => (
-  (handle1 || '').toLowerCase() === (handle2 || '').toLowerCase()
-)
+export const compareHandles = (handle1, handle2) => {
+  if (handle2.indexOf('@') === 0) {
+    return (handle1 || '').toLowerCase() === (handle2.substring(1) || '').toLowerCase()
+  }
+  return (handle1 || '').toLowerCase() === (handle2 || '').toLowerCase()
+}
 
 // remove empty object, null/undefined value and empty string recursively from passed obj
 const deepClean = obj => _.transform(obj, (result, value, key) => {
