da95bbd3ecc9efd71c59e3a518a06445d8631fc0
gets0ul <gets0ul@gmail.com>
2019-09-24 19:37:27 +0700

    Set error flag as false as it is actually not needed.

--- src/reducers/templates.js
+++ src/reducers/templates.js
@@ -165,14 +165,14 @@ export default function(state = initialState, action) {
     return {
       ...state,
       isLoading: false,
-      error: action.payload.response.data.result.status >= 500
+      error: false
     }
   case UPDATE_PROJECTS_METADATA_FAILURE:
     Alert.error(`PROJECT METADATA UPDATE FAILED: ${action.payload.response.data.result.content.message}`)
     return {
       ...state,
       isLoading: false,
-      error: action.payload.response.data.result.status >= 500
+      error: false
     }
   case REMOVE_PROJECTS_METADATA_FAILURE:
   case REMOVE_PRODUCT_CATEGORY_FAILURE:
@@ -183,7 +183,7 @@ export default function(state = initialState, action) {
     return {
       ...state,
       isRemoving: false,
-      error: action.payload.response.data.result.status >= 500
+      error: false
     }
   case ADD_PROJECTS_METADATA_SUCCESS:
   case CREATE_PROJECT_TEMPLATE_SUCCESS:
