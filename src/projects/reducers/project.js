ef69aa4f924babc574150859ac0243ca97af3c24
Maksym Mykhailenko <maxceem@gmail.com>
2019-12-02 18:00:03 +0800

    fix: approve/decline copilot

--- src/projects/reducers/project.js
+++ src/projects/reducers/project.js
@@ -209,7 +209,6 @@ export const projectState = function (state=initialState, action) {
 
   case ACCEPT_OR_REFUSE_INVITE_PENDING:
     return Object.assign({}, state, {
-      showUserInvited: true,
       inviteError: false,
     })
 
