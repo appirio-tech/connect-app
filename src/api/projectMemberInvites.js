75733a4a5ac80bd74cc5d6e785ddc57f64769bf1
Maksym Mykhailenko <maxceem@gmail.com>
2019-12-14 15:48:24 +0800

    fix: member invite after list redesign

--- src/api/projectMemberInvites.js
+++ src/api/projectMemberInvites.js
@@ -22,7 +22,8 @@ export function updateProjectMemberInvite(projectId, member) {
  * @return {object}  project member invite returned by api
  */
 export function createProjectMemberInvite(projectId, member) {
-  const url = `${PROJECTS_API_URL}/v4/projects/${projectId}/members/invite/`
+  const fields = 'id,projectId,userId,email,role,status,createdAt,updatedAt,createdBy,updatedBy,handle,firstName,lastName,photoURL'
+  const url = `${PROJECTS_API_URL}/v4/projects/${projectId}/members/invite/?fields=` + encodeURIComponent(fields)
   return axios({
     method: 'post',
     url,
