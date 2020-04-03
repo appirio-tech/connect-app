98a676085aaac7b894c7508fba1b8296b1407ec4
Maksym Mykhailenko <maxceem@gmail.com>
2020-04-03 09:37:37 +0300

    fix: file download
    ref issue #3839

--- src/components/FileDownload.jsx
+++ src/components/FileDownload.jsx
@@ -8,7 +8,6 @@ class FileDownload extends React.Component {
   constructor(props) {
     super(props)
     this.state={loaded:false, error:null}
-    
   }
 
   componentWillMount() {
@@ -26,7 +25,7 @@ class FileDownload extends React.Component {
     } else {
       const projectId = this.props.match.params.projectId
       const attachmentId = this.props.match.params.attachmentId
-      getProjectAttachment(projectId, attachmentId).then((url) => {
+      getProjectAttachment(projectId, attachmentId).then(({ url }) => {
         window.location = url
       }).catch(() => {
         this.setState({loaded:true, error:'File unavailable'})
@@ -35,7 +34,7 @@ class FileDownload extends React.Component {
   }
 
   render() {
-    
+
 
     return (
       <div >
