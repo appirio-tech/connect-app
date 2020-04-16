aa93c2ed1902661f98616b5007058f536576a7d4
prakashdurlabhji <prakashseta@gmail.com>
2020-04-16 05:30:11 +0530

    issue 3868 fix

--- src/projects/components/projectsCard/ProjectCardBody.jsx
+++ src/projects/components/projectsCard/ProjectCardBody.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PT from 'prop-types'
 import TextTruncate from 'react-text-truncate'
-import { Link } from 'react-router-dom'
 import ProjectStatus from '../../../components/ProjectStatus/ProjectStatus'
 import editableProjectStatus from '../../../components/ProjectStatus/editableProjectStatus'
 import {
@@ -16,39 +15,63 @@ import _ from 'lodash'
 
 const EnhancedProjectStatus = editableProjectStatus(ProjectStatus)
 
-function ProjectCardBody({ project, projectCanBeActive, currentMemberRole, descLinesCount = 8,
-  onChangeStatus, isSuperUser, showLink, showLinkURL, canEditStatus = true, hideStatus }) {
-  if (!project) return null
+class ProjectCardBody extends React.Component {
+    constructor (props) {
+      super(props)
+      this.state = {
+        redirectToDetails: false,
+        redirectToUrl: ''
+      }
 
-  const canEdit = canEditStatus && (
-    project.status !== PROJECT_STATUS_COMPLETED && (isSuperUser || (currentMemberRole
-    && (_.indexOf([
-      PROJECT_ROLE_COPILOT,
-      PROJECT_ROLE_MANAGER,
-      PROJECT_ROLE_PROGRAM_MANAGER,
-      PROJECT_ROLE_PROJECT_MANAGER,
-      PROJECT_ROLE_SOLUTION_ARCHITECT,
-    ], currentMemberRole) > -1)))
-  )
+      this.redirectTo = this.redirectTo.bind(this)
+    }
 
-  const progress = _.get(process, 'percent', 0)
+    redirectTo(evt, link) {
+      evt.preventDefault()
+      evt.stopPropagation()
+      this.setState({redirectToDetails: true, redirectToUrl: link});
+    }
 
-  const projectDetailsURL = project.version === 'v3'
+  render () {
+    const { project, projectCanBeActive, currentMemberRole, descLinesCount = 8, 
+      onChangeStatus, isSuperUser, showLink, showLinkURL, canEditStatus = true, hideStatus } = this.props
+    
+    if (!project) return null
+
+    if (this.state.redirectToDetails) {
+      return <Redirect push to={this.state.redirectToUrl} />;
+    }
+    
+    const canEdit = canEditStatus && (
+      project.status !== PROJECT_STATUS_COMPLETED && (isSuperUser || (currentMemberRole
+        && (_.indexOf([
+          PROJECT_ROLE_COPILOT,
+          PROJECT_ROLE_MANAGER,
+          PROJECT_ROLE_PROGRAM_MANAGER,
+          PROJECT_ROLE_PROJECT_MANAGER,
+          PROJECT_ROLE_SOLUTION_ARCHITECT,
+        ], currentMemberRole) > -1)))
+        )
+        
+    const progress = _.get(process, 'percent', 0)
+    
+    const projectDetailsURL = project.version === 'v3'
     ? `/projects/${project.id}/scope`
     : `/projects/${project.id}/specification`
-
-  return (
-    <div className="project-card-body">
-      <TextTruncate
-        containerClassName="project-description"
-        line={descLinesCount}
-        truncateText="..."
-        text={_.unescape(project.description)}
-        textTruncateChild={showLink ? <Link className="read-more-link" to={showLinkURL || projectDetailsURL}>read more</Link> : <span className="read-more-link">read more</span>}
-      />
-      {!hideStatus && <div className="project-status">
-        {(project.status !== PROJECT_STATUS_ACTIVE || progress === 0) &&
-          <EnhancedProjectStatus
+    const readMoreLink = showLinkURL || projectDetailsURL
+    return (
+      <div className="project-card-body">
+        <TextTruncate
+          containerClassName="project-description"
+          line={descLinesCount}
+          truncateText="..."
+          text={_.unescape(project.description)}
+          
+          textTruncateChild={showLink ? <li key={project.id} className="read-more-link" onClick={() => this.redirectTo(evt, readMoreLink)}>read more</li> : <span className="read-more-link">read more</span>}
+          />
+        {!hideStatus && <div className="project-status">
+          {(project.status !== PROJECT_STATUS_ACTIVE || progress === 0) &&
+            <EnhancedProjectStatus
             status={project.status}
             projectCanBeActive={projectCanBeActive}
             showText
@@ -58,11 +81,12 @@ function ProjectCardBody({ project, projectCanBeActive, currentMemberRole, descL
             unifiedHeader={false}
             onChangeStatus={onChangeStatus}
             projectId={project.id}
-          />
-        }
-      </div>}
-    </div>
-  )
+            />
+          }
+        </div>}
+      </div>
+    )
+  }
 }
 
 ProjectCardBody.defaultTypes = {
