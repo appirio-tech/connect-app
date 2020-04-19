c68e3c94bc23848fdb8e9e3ffb1d16be123801db
Romit Choudhary <romit.choudhary@rivigo.com>
2020-04-19 12:47:12 +0530

    formatting changes reversed

--- src/components/TeamManagement/AutocompleteInputContainer.jsx
+++ src/components/TeamManagement/AutocompleteInputContainer.jsx
@@ -3,13 +3,11 @@ import { connect } from 'react-redux'
 import _ from 'lodash'
 
 import AutocompleteInput from './AutocompleteInput'
-import {
-  memberSuggestionsDispatch,
-  clearMemberSuggestions,
-} from '../../projects/actions/projectMember'
-import { AUTOCOMPLETE_TRIGGER_LENGTH } from '../../config/constants'
+import {memberSuggestionsDispatch, clearMemberSuggestions} from '../../projects/actions/projectMember'
+import {AUTOCOMPLETE_TRIGGER_LENGTH} from '../../config/constants'
 
 class AutocompleteInputContainer extends React.Component {
+
   constructor(props) {
     super(props)
     this.debounceTimer = null
@@ -31,20 +29,18 @@ class AutocompleteInputContainer extends React.Component {
   }
 
   onInputChange(inputValue) {
+    const { currentUser } = this.props
     const indexOfSpace = inputValue.indexOf(' ')
     const indexOfSemiColon = inputValue.indexOf(';')
 
     // if user enter only ' '  or ';' we should clean it to not allow
-    if (indexOfSpace === 0 || indexOfSemiColon === 0) {
+    if (indexOfSpace === 0 || indexOfSemiColon === 0 ) {
       return ''
     }
 
-    if (indexOfSpace >= 1 || indexOfSemiColon >= 1) {
-      inputValue = inputValue.substring(0, inputValue.length - 1)
-      this.onUpdate([
-        ...this.props.selectedMembers,
-        { label: inputValue, value: inputValue },
-      ])
+    if (indexOfSpace >= 1 || indexOfSemiColon >= 1 ) {
+      inputValue = inputValue.substring(0, inputValue.length -1 )
+      this.onUpdate([...this.props.selectedMembers, {label: inputValue, value: inputValue}])
       this.clearUserSuggestions()
       // this is return empty to nullify inputValue post processing
       return ''
@@ -61,9 +57,9 @@ class AutocompleteInputContainer extends React.Component {
   }
 
   onUpdate(inputValue) {
-    const inputValueNormalized = inputValue.map((value) => ({
+    const inputValueNormalized = inputValue.map(value => ({
       ...value,
-      isEmail: /(.+)@(.+){2,}\.(.+){2,}/.test(value.label),
+      isEmail: (/(.+)@(.+){2,}\.(.+){2,}/).test(value.label)
     }))
 
     if (this.props.onUpdate) {
@@ -73,11 +69,12 @@ class AutocompleteInputContainer extends React.Component {
   }
 
   render() {
+
     const { placeholder, currentUser, selectedMembers, disabled } = this.props
 
     return (
       <AutocompleteInput
-        placeholder={placeholder ? placeholder : ''}
+        placeholder={placeholder ? placeholder:''}
         onInputChange={this.onInputChange.bind(this)}
         onUpdate={this.onUpdate.bind(this)}
         suggestedMembers={this.props.suggestedMembers}
@@ -91,32 +88,25 @@ class AutocompleteInputContainer extends React.Component {
 
 const mapStateToProps = (reduxstore) => {
   return {
-    suggestedMembers: reduxstore.members.suggestedMembers.map((suggestion) => {
-      return {
-        label: suggestion.handle,
-        value: suggestion.handle,
-      }
-    }),
+    suggestedMembers: reduxstore.members.suggestedMembers
+      .map(suggestion => {
+        return {
+          label: suggestion.handle, value: suggestion.handle
+        }
+      })
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  const debouncedDispatcher = _.debounce(
-    (arg) => memberSuggestionsDispatch(dispatch)(arg),
-    500,
-    { leading: true }
-  )
+  const debouncedDispatcher = _.debounce((arg) => memberSuggestionsDispatch(dispatch)(arg), 500, {leading: true})
   return {
     onLoadUserSuggestions: (value) => {
       debouncedDispatcher(value)
     },
     onClearUserSuggestions: () => {
       clearMemberSuggestions(dispatch)
-    },
+    }
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(AutocompleteInputContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(AutocompleteInputContainer)
