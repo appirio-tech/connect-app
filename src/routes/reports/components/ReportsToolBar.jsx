d46b8d1fc81f684506aae270cd1bfb8d433043bf
maxceem <maxceem@gmail.com>
2020-09-14 13:49:40 +0300

    fix: open reports tab after searching
    ref issue #4069

---
+++ src/routes/reports/components/ReportsToolBar.jsx
@@ -0,0 +1,11 @@
+/**
+ * Reports pages tool bar
+ */
+import React from 'react'
+import SectionTopBar from '../../../components/TopBar/SectionToolBar'
+
+const ReportsToolBar = () => (
+  <SectionTopBar title="Reports" menu={[]} />
+)
+
+export default ReportsToolBar
