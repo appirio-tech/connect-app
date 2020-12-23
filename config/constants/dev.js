d545213e691a206f632bfa72bdf7fb5366cb08d5
maxceem <maxceem@gmail.com>
2020-12-23 14:19:36 +0200

    chore: set production phase product template

--- config/constants/dev.js
+++ config/constants/dev.js
@@ -43,7 +43,7 @@ module.exports = {
   PREDIX_PROGRAM_ID         : 3448,
   IBM_COGNITIVE_PROGRAM_ID  : 3449,
   HEAP_ANALYTICS_APP_ID     : '4153837120',
-  PHASE_PRODUCT_TEMPLATE_ID : 166,
+  PHASE_PRODUCT_TEMPLATE_ID : 27,
 
   TC_NOTIFICATION_URL: 'https://api.topcoder-dev.com/v5/notifications',
   CONNECT_MESSAGE_API_URL: 'https://api.topcoder-dev.com/v5',
