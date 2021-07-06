45b5a783ba152b58dce76e1368f03b3fc6f416af
tc_user <vijay.86@wipro.com>
2021-07-07 02:01:57 +0800

    add taas project creation test

--- connect-automation/page-objects/project-creation-flow/create-project/create-project.model.ts
+++ connect-automation/page-objects/project-creation-flow/create-project/create-project.model.ts
@@ -5,11 +5,23 @@ export interface IProjectData {
   notes: string;
 }
 
+export interface ITaasData {
+  title: string;
+  jobTitle: string;
+  numOfPeople: number;
+  duration: number;
+  description: string;
+  email: string;
+  answers: IAnswers;
+}
+
 export interface IAnswers {
   beforeWeStart: string;
-  whatDoYouNeed: string;
-  willYourAppNeedMoreScreen: string;
-  howManyScreens: string;
-  whereShouldAppWork: string;
-  howShouldAppWorks: string;
+  whatDoYouNeed?: string;
+  willYourAppNeedMoreScreen?: string;
+  howManyScreens?: string;
+  whereShouldAppWork?: string;
+  howShouldAppWorks?: string;
+  startDate?: string;
+  requirement?: string;
 }
